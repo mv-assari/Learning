cd162845154487b00c2977439aed08cab4583fd2
Mva <m.vahidassari@gmail.com>
2026-02-22 06:18:35 +0330

    add file for explain

---
+++ Asp.Net-Core/1-Linq/Partitioning/Partitioning/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Partitioning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IList<int> data=new List<int> { 5, 2, 4, 7, 8, 1 };
+
+            var skipResult = data.Skip(2);
+
+            var skipWhileResult = data.SkipWhile(r => r > 4);
+
+            var takeResult=data.Take(2);
+
+            var takeWhileResult=data.TakeWhile(x=>x> 4);
+
+            Console.ReadKey();
+        }
+    }
+}
