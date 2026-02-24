18c98f2528c65131e343a092ffb7382d6750783b
Mva <mva@gmail.com>
2026-02-25 02:23:34 +0330

    فایل های مورد نیاز برای آموزش

---
+++ Asp.Net-Core/2-UnitTest/2- XUnit/XUnit/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace XUnit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MathHelper mathHelper=new MathHelper();
+            while (true)
+            {
+                Console.WriteLine("X re vared konid:");
+                int x=Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Y ra vared konid:");
+                int y=Convert.ToInt32(Console.ReadLine());
+
+                var result= mathHelper.Jam(x,y);
+
+                Console.WriteLine($"X:{x} + Y:{y} = {result}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Asp.Net-Core/2-UnitTest/2- XUnit/XUnit/TestData/MemberClassData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XUnitLearn.TestData
+{
+    public class MemberClassData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] { 4, 4, 8 };
+            yield return new object[] { 24, 24, 48 };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
