aa7f7fc633b35576c3c2080de612462a63502ac4
Mva <mva@gmail.com>
2026-02-27 00:16:38 +0330

    موارد مورد در کامیت اضافه شد

---
+++ Asp.Net-Core/2-UnitTest/4- UnitTest for WebApi/WebAppApi.Test/ProductApiControllerTest.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAppApi.Controllers;
+using WebAppMvc.Models;
+using WebAppMvc.Models.Entities;
+using WebAppMvc.Models.Services;
+using Xunit;
+
+namespace WebAppApi.Test
+{
+    public class ProductApiControllerTest
+    {
+        MockData mockData;
+        public ProductApiControllerTest()
+        {
+            mockData = new MockData();
+        }
+
+        [Fact]
+        public void Get_Test()
+        {
+            //Arrange
+            var moq = new Mock<IProductService>();
+            moq.Setup(p => p.GetAll()).Returns(mockData.GetAll());
+
+            ProductApiController productApi=new ProductApiController(moq.Object);
+
+            //Act
+            var result= productApi.Get();
+
+            //Assert
+            Assert.IsType<OkObjectResult>(result);
+            var list=result as OkObjectResult;
+            Assert.IsType<List<Product>>(list.Value);
+        }
+
+        [Theory]
+        [InlineData(1,-1)]
+        public void GetById_Test(int validId, int inValidId)
+        {
+            var moq =new Mock<IProductService>();
+            moq.Setup(p => p.GetById(validId)).Returns(mockData.GetAll().FirstOrDefault(p=>p.Id==validId));
+
+            ProductApiController productApi= new ProductApiController(moq.Object);
+
+            var result= productApi.Get(validId);
+
+            Assert.IsType<OkObjectResult>(result);
+            var product=result as OkObjectResult;
+            Assert.IsType<Product>(product.Value);
+
+            //--------------------------------
+
+            moq.Setup(p => p.GetById(inValidId)).Returns(mockData.GetAll().FirstOrDefault(p=>p.Id==inValidId));
+
+            var inValidIdResult=productApi.Get(inValidId);
+
+            Assert.IsType<NotFoundResult>(inValidIdResult);
+
+        }
+
+        [Fact]
+        public void Post_Test()
+        {
+            Product product = new Product
+            {
+                Id = 1,
+                Name = "test",
+                Description = "test",
+                Price = 2000
+            };
+
+            var moq = new Mock<IProductService>();
+            moq.Setup(p => p.Add(product)).Returns(mockData.GetAll().FirstOrDefault());
+
+            ProductApiController productApi=new ProductApiController(moq.Object);
+
+            var result=productApi.Post(product);
+
+            Assert.IsType<CreatedAtActionResult>(result);
+        }
+
+        [Theory]
+        [InlineData(1,-1)]
+        public void Delete_Test(int validId,int inValidId)
+        {
+            var moq=new Mock<IProductService>();
+            moq.Setup(p => p.GetById(validId)).Returns(mockData.GetAll().FirstOrDefault());
+            moq.Setup(p => p.Remove(validId));
+
+            ProductApiController productApi= new ProductApiController(moq.Object);
+
+            var result= productApi.Delete(validId);
+
+            Assert.IsType<OkObjectResult>(result);
+
+            //-----------------------------------------
+
+            moq.Setup(p=>p.Remove(inValidId));
+
+            var resultInvalid = productApi.Delete(inValidId);
+
+            Assert.IsType<NotFoundResult>(resultInvalid);
+        }
+    }
+}
---
+++ Asp.Net-Core/2-UnitTest/4- UnitTest for WebApi/WebAppApi/Controllers/ProductApiController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using WebAppMvc.Models.Entities;
+using WebAppMvc.Models.Services;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAppApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductApiController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        public ProductApiController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+
+        // GET: api/<ProductApiController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_productService.GetAll());
+        }
+
+        // GET api/<ProductApiController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var product = _productService.GetById(id);
+            if(product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        // POST api/<ProductApiController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Product value)
+        {
+            var result = _productService.Add(value);
+
+            return CreatedAtAction("Get", new { id = result.Id });
+        }
+
+        // DELETE api/<ProductApiController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var product= _productService.GetById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _productService.Remove(id);
+            return Ok(true);
+        }
+    }
+}
