432912ac9e1e1e6faf15c79b536627139fb55543
Mva <mva@gmail.com>
2026-02-27 03:18:45 +0330

    موارد مورد نیاز در کامیت اضافه شد

---
+++ Asp.Net-Core/2-UnitTest/6- Mock(moq)/WebApp/Services/CarService.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApp.Services
+{
+    public class CarService
+    {
+        protected virtual int GetCarPrice()
+        {
+            return 1000000;
+        }
+    }
+}
---
+++ Asp.Net-Core/2-UnitTest/6- Mock(moq)/WebApp/Services/IProdcutService.cs
@@ -0,0 +1,23 @@
+﻿namespace WebApp.Services
+{
+    public interface IBrand
+    {
+        public int Id { get; set; }
+    }
+
+    public interface IProdcutService
+    {
+        int GetProductPrice(int id);
+        void GetProductPrice(int id,out int price);
+
+        int ProductCount { get; set; }
+        Product AddProduct(Product product);
+        IBrand Brand { get; set; }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
