9cc6bee53cbd23b86827140dbd9045703e3efe29
Mva <mva@gmail.com>
2026-02-27 01:17:20 +0330

    موارد مورد نیاز در کامیت اضافه شد

---
+++ Asp.Net-Core/2-UnitTest/5- TDD/TDD.Test/SumTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TDD.Models;
+using Xunit;
+
+namespace TDD.Test
+{
+    public class SumTest
+    {
+        [Theory]
+        [InlineData("1,2",3)]
+        [InlineData("",0)]
+        [InlineData("1,2,4",7)]
+        [InlineData("1,2,4,",7)]
+        [InlineData("0",0)]
+        public void Execute_SumNumber_When_StringNumber(string numbers,int expected)
+        {
+            //Arrange
+            Sum sum = new Sum();
+
+            //Act
+
+            var result=sum.Execute(numbers);
+
+            //Assert
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ Asp.Net-Core/2-UnitTest/5- TDD/TDD/Models/Sum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+
+namespace TDD.Models
+{
+    public class Sum
+    {
+        public int Execute(string numbers)
+        {
+            if (numbers=="")
+                return 0;
+            
+            if(numbers=="0")
+                 return 0; 
+
+            if (numbers.EndsWith(','))
+            {
+                numbers=numbers.Substring(0,numbers.Length-1);
+            }
+            string[] spnum=numbers.Split(',');
+
+            int[] intNumbers=Array.ConvertAll(spnum,s=>int.Parse(s));
+
+            return intNumbers.Sum();
+        }
+    }
+}
