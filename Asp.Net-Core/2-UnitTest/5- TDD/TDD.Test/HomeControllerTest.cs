dfa598434f51d26b0ac72583f21657a0c4c3b679
Mva <m.vahidassari@gmail.com>
2026-02-27 01:17:20 +0330

    موارد مورد نیاز در کامیت اضافه شد

---
+++ Asp.Net-Core/2-UnitTest/5- TDD/TDD.Test/HomeControllerTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TDD.Controllers;
+using Xunit;
+
+namespace TDD.Test
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void Index_Test()
+        {
+            //Arrange
+
+            HomeController controller =new HomeController();
+
+            //Act
+
+            var result= controller.Index();
+
+            //Assert
+
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
