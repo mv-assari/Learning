5773c716e69cadfb91da28f20e0d8c35a67c3a11
Mva <m.vahidassari@gmail.com>
2026-02-26 03:38:53 +0330

    موارد مورد نیاز در کامیت اضافه شد.

---
+++ Asp.Net-Core/2-UnitTest/3- UnitTest for Core MVC/WebAppMvc/Models/Entities/Product.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppMvc.Models.Entities
+{
+    public class Product
+    {
+        public long Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ Asp.Net-Core/2-UnitTest/3- UnitTest for Core MVC/WebAppMvc/Models/MockData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using WebAppMvc.Models.Entities;
+
+namespace WebAppMvc.Models
+{
+    public class MockData
+    {
+        public List<Product> GetAll()
+        {
+            List<Product> products = new List<Product>
+            {
+                new Product{Id=1,Name="Iphone1",Description="In Category Mobile",Price=15000},
+                new Product{Id=2,Name="Iphone2",Description="In Category Mobile",Price=25000},
+                new Product{Id=3,Name="Iphone3",Description="In Category Mobile",Price=35000},
+                new Product{Id=4,Name="Iphone4",Description="In Category Mobile",Price=45000},
+            };
+
+            return products;
+        }
+
+    }
+}
