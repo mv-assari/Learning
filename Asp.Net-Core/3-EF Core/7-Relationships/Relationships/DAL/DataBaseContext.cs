f22b45cf61338ed35da446ab57ed03a34fc8e79d
Mva <m.vahidassari@gmail.com>
2026-05-03 23:08:05 +0330

    add file for explain

---
+++ Asp.Net-Core/3-EF Core/7-Relationships/Relationships/DAL/DataBaseContext.cs
@@ -0,0 +1,58 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DataBaseContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"server=.;initial catalog=EfTest;integrated security=true;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Category>().HasMany(p => p.Products).WithOne(p => p.Category);
+
+            //modelBuilder.Entity<Product>().HasOne(p => p.Category).WithMany(p => p.Products);
+
+            //modelBuilder.Entity<Category>().HasMany(c => c.Products).WithOne();
+
+
+            //اگر خودمون به صورت دستی بخوایم کلید خارجی تعریف کنیم
+            //modelBuilder.Entity<Category>()
+            //    .HasMany(c => c.Products)
+            //    .WithOne(p => p.Category)
+            //    .HasForeignKey(p => p.FK_CategoryId)
+            //    .HasConstraintName("FK_C_P");
+
+
+            //اگر از هیج قراردادی و پراپرتی برای ارتباط بین دو جدول استفاده نکنیم و به صورت دستی بخواهیم انجام بدیم
+            //modelBuilder.Entity<Product>()
+            //    .HasOne<Category>()
+            //    .WithMany()
+            //    .HasForeignKey(p => p.FK_CategoryId)
+            //    .IsRequired(false); //اگر بخواهیم کلید خارجی را غیراجباری تعریف کنیم یا جلوی پراپرتی ؟ میزاریم
+
+            //در این قسمت میتوانیم که نوع حذف را مشخص کنیم
+            //modelBuilder.Entity<Product>()
+            //    .HasOne(p=>p.Category)
+            //    .WithMany(c=>c.Products)
+            //    .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Product>()
+                .HasOne(p => p.ProductImage)
+                .WithOne(i => i.Product)
+                .HasPrincipalKey<ProductImage>(i => i.FK_ProductId);
+        }
+    }
+}
---
+++ Asp.Net-Core/3-EF Core/7-Relationships/Relationships/Entities/Product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+
+    public class Category
+    {
+        public long Id { get; set; }
+        public string CategoryName { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+
+    public class Product
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public Category Category { get; set; }
+        public long CategoryId { get; set; }
+
+        public ProductImage ProductImage { get; set; }
+
+    }
+
+    public class ProductImage
+    {
+        public long Id { get; set; }
+        public byte[] Image { get; set; }
+        public Product Product { get; set; }
+        //public long ProductId { get; set; }
+        public long FK_ProductId { get; set; }
+    }
+}
