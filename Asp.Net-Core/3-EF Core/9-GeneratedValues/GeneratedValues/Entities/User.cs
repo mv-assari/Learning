f97f7550e09f497b290d6f336c5ddea4ecba5c7d
Mva <m.vahidassari@gmail.com>
2026-05-04 15:29:16 +0330

    add file for explain

---
+++ Asp.Net-Core/3-EF Core/9-GeneratedValues/GeneratedValues/Entities/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
+        public int Score { get; set; }
+        public DateTime InsertDate { get; set; }
+        public ICollection<Order> Orders { get; set; }
+    }
+
+    public class Order
+    {
+        public long Id { get; set; }
+        public string OrderNumber { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ Asp.Net-Core/3-EF Core/9-GeneratedValues/GeneratedValues/Ui/Program.cs
@@ -0,0 +1,30 @@
+﻿using DAL;
+using Entities;
+using System;
+
+namespace Ui
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DataBaseContext context = new DataBaseContext();
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            User user = new User()
+            {
+               // Id= 1,
+                FirstName = "vahid",
+                LastName = "Assari",
+            };
+
+            context.Users.Add(user);
+            context.SaveChanges();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
