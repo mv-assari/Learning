49d78a1c1d3120dde23f75c965a3141449d03bd9
Mva <m.vahidassari@gmail.com>
2026-05-16 14:24:37 +0330

    add file for explain

---
+++ C#/1-C# Advance/3-AnonymousMethods/AnonymousMethods/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AnonymousMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product{Id=1,Name="M1"},
+                new Product{Id=2,Name="M2"},
+
+            };
+
+            var result = products.Where(
+            
+                delegate (Product p)
+                    {
+                        return p.Id == 1;
+                    }
+             );
+
+            Console.WriteLine($"{result.ToList().FirstOrDefault().Name}");
+            Console.ReadLine();
+        }
+
+        public static bool GetProduct(Product product)
+        {
+            if (product.Id==1)
+                return true;
+            return false;
+        }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
