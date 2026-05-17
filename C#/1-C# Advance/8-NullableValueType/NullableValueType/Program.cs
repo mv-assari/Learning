da1bb4a03abbcbfb3e610533d81bad6eb7711576
Mva <m.vahidassari@gmail.com>
2026-05-17 16:30:45 +0330

    add file for explain

---
+++ C#/1-C# Advance/8-NullableValueType/NullableValueType/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NullableValueType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Nullable<int> num = null;
+            int a=num.GetValueOrDefault();
+
+            Nullable<bool> boolvalue;
+            boolvalue = null;
+            boolvalue = boolvalue.GetValueOrDefault();
+
+            bool? boolvalue2 = true;//با این علامت (؟) هم میتوان کار دستورات بالا را انجام داد 
+
+            Console.WriteLine(boolvalue2.HasValue);
+            boolvalue2=boolvalue2.GetValueOrDefault();
+
+            Console.ReadLine();
+        }
+    }
+}
