bf4fed36cfe5b9484b794ac40de153b6675f82f5
Mva <m.vahidassari@gmail.com>
2026-05-18 15:11:44 +0330

    add file for explain

---
+++ C#/1-C# Advance/13-InitOnly/InitOnly/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace InitOnly
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product()
+            {
+                Id = 1
+            };
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Product
+    {
+        //توسط این نوع میتوان فقط یک مرتبه به پراپرتی مقداری اختصاص داد
+        public int Id { get; init; }
+    }
+}
