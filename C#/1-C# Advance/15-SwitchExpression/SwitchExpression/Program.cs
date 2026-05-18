542ae975d5e2b79ead7ae7934971b2eadb96c71f
Mva <m.vahidassari@gmail.com>
2026-05-18 16:14:40 +0330

    add file for explain

---
+++ C#/1-C# Advance/15-SwitchExpression/SwitchExpression/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SwitchExpression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sitename = "mva";
+
+            var result = sitename switch
+            {
+                "mva" => 2,
+                "ali" => 3,
+                _ => 4 //default value
+            };
+
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
