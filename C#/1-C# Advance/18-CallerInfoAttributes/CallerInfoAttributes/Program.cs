db52d28f0e514404d48e47240c2ec1b52aae38b4
Mva <m.vahidassari@gmail.com>
2026-05-19 12:53:01 +0330

    add file for explain

---
+++ C#/1-C# Advance/18-CallerInfoAttributes/CallerInfoAttributes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace CallerInfoAttributes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Utitlity.MyMethod();
+            Console.ReadLine();
+        }
+    }
+
+    public static class Utitlity
+    {
+        public static void MyMethod(
+            [CallerMemberName] string membername = null,
+            [CallerFilePath] string filepath = null,
+            [CallerLineNumber] int linenumber=0)
+        {
+            Console.WriteLine("MyMethod Run .... ");
+            Console.WriteLine($"Method:{membername}");
+            Console.WriteLine($"FilePath:{filepath}");
+            Console.WriteLine($"LineNumber:{linenumber}");
+        }
+    }
+}
