fd9cece839f9f5412bed34edfc6fc3c9078691cb
Mva <m.vahidassari@gmail.com>
2026-05-19 15:10:09 +0330

    add file for explain

---
+++ C#/2-NewFeatureInC#10/1-NewStructureConsoleInDotNet6/NewStructureConsoleInDotNet6/Program.cs
@@ -0,0 +1,7 @@
+﻿// in dotnet6 we have just one top level statement
+// all of the using are in globalusing in root project
+Console.WriteLine("Hello, World!");
+if (true)
+{
+    Console.WriteLine("we have true!");
+}
