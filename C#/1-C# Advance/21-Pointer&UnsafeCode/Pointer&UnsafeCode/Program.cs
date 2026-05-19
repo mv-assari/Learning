90151bc5735a308dadc14a371ea97687ebddd818
Mva <m.vahidassari@gmail.com>
2026-05-19 14:22:04 +0330

    add file for explain

---
+++ C#/1-C# Advance/21-Pointer&UnsafeCode/Pointer&UnsafeCode/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Pointer_UnsafeCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            unsafe
+            {
+                int number = 154;
+                int* p = &number;
+
+                Console.WriteLine($"number:{number}");
+                Console.WriteLine($"address number:{(int)p}");
+                Console.WriteLine($"data p:{p->ToString()}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
