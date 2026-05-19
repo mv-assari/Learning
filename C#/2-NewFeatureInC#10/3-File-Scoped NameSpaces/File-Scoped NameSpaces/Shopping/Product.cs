f046d9fca1bd3ea04833f2b1da773de92a7e6ca6
Mva <m.vahidassari@gmail.com>
2026-05-19 21:46:30 +0330

    add file for explain

---
+++ C#/2-NewFeatureInC#10/3-File-Scoped NameSpaces/File-Scoped NameSpaces/Shopping/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_Scoped_NameSpaces.Shopping; //حتی میتونیم برای ساده سازی به این صورت تعریف کنیم ولی دیگر نمیتوانیم در یک فایل  فضای نام های تودرتو داشته باشیم و خطا میده
+    public class Product
+    {
+    }
+
+
+//namespace File_Scoped_NameSpaces.Shopping
+//{
+//    namespace Product
+//    {
+//        namespace Order
+//        {
+//            // به این صورت هم حتی میتونیم تودرتو تعریف کنیم
+//        }
+//    }
+//}
+
