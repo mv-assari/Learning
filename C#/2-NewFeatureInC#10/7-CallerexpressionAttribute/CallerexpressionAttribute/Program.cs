1a647ffb319f9daa933e0e81a7c46b7af6847a52
Mva <m.vahidassari@gmail.com>
2026-05-20 14:25:56 +0330

    add file for explain

---
+++ C#/2-NewFeatureInC#10/7-CallerexpressionAttribute/CallerexpressionAttribute/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.CompilerServices;
+
+Console.WriteLine("Hello, World!");
+void Test(bool condition ,[CallerArgumentExpression("condition")] string data=null) //توسط این قابلیت میتوان دیتای ورودی رو هم لاگ کرد
+{
+    Console.WriteLine($"condition:{condition}");
+    Console.WriteLine($"data:{data}");
+}
+
+
+
+int a = 1;
+int b = 1;
+Test(a == b);
+Test(true);
+Test(1 == 1);
+
+Console.ReadLine();
+
