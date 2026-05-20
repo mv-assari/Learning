43709f4d10ec324effaf5704c24720cafa1900e0
Mva <m.vahidassari@gmail.com>
2026-05-20 11:26:46 +0330

    add file for explain

---
+++ C#/2-NewFeatureInC#10/4-Structs/Structs/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+public struct Address
+{
+    public Address(string city)
+    {
+        City = city;
+    }
+
+    public string City { get; set; } = "UnKnown";
+    public string State { get; set; }="UnKnown";
+    public string PhoneNumber { get; set; } = "UnKnown";
+}
