a9ba4ccf28fb6fa27e02a6adcdf3b580dca7ae4e
Mva <m.vahidassari@gmail.com>
2026-05-20 12:18:22 +0330

    add file for explain

---
+++ C#/2-NewFeatureInC#10/5-Records/Records/Program.cs
@@ -0,0 +1,33 @@
+﻿
+var r1 = new MyRecored(2, 3);
+(int x, int y) = r1;
+
+Console.WriteLine(r1);
+
+var r2 = new Myr2(3,3);
+//r2.a = 3;
+
+var r2v2 = r2 with { a = 3 };//استفاده از کلمه کلیدی ویث برای کپی کردن تمام مقادیر قبلی و تغییرات در مقادبر خاص در متغیر جدید
+public record class MyRecored(int a,int b); //کلمه کلیدی کلاس در این قسمت اختیاری هست
+
+//--------------------------
+
+// کلمه کلیدی استراکت در این قسمت اجباری هست
+//اگر کلمه کلیدی فقط خواندنی را بگزاریم پراپرتی ها فقط هنگام تعریف قابلیت مقداردهی دارند
+public readonly record struct Myr2(int a,int b); 
+
+
+//کدهای زیر نمایانگر این هستند که رکورد ها قابلیت ارث بری را دارند
+//با کلمه کلیدی سیلد میتوان قابلیت کلاس پدر را از کلاس فرزند گرفت
+public record ProdcutDto
+{
+    public sealed override string ToString()
+    {
+        return "---";
+    }
+}
+
+public record ProdcutDetailDto:ProdcutDto
+{
+   
+}
---
+++ C#/2-NewFeatureInC#10/5-Records/Records/WithSample.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Records
+{
+    public class WithSample
+    {
+        public void Execute()
+        {
+            UserDto user = new UserDto();
+            user.Id = 1;
+            user.Name= "Test";
+            user.Email = "mva@gmail.com";
+
+            var user2 = user with { Name = "vahid" };//استفاده از کلمه کلیدی ویث در استراکت ها
+
+            var person = new { Id = 2, Name = "mva" };
+            var person2 = person with { Name = "Assari" };// استفاده از کلمه کلیدی ویث برای متغیرهای بی نام
+        }
+
+    }
+
+    public struct UserDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
