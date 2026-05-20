44f5dd4e0ba9afefdac46f88ab249f5de43df32b
Mva <m.vahidassari@gmail.com>
2026-05-20 14:32:54 +0330

    add file for explain

---
+++ C#/2-NewFeatureInC#10/8-Assignment&DeclaratoinInSameDeconstruction/Assignment&DeclaratoinInSameDeconstruction/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Point point = new Point(10, 20);
+
+int x;
+(x, int y) = point; //در این حالت میتوان از قبل متغیر تعریف شود و در این جا از آن استفاده شود
+
+public record Point(int a,int b);
