1bfce52242d2c3cc500049c5cc9cca54242674ea
Mva <m.vahidassari@gmail.com>
2026-05-21 17:14:55 +0330

    add file for explain

---
+++ C#/3-FrameworkFundamentals/4-TimeSpan/TimeSpan/Program.cs
@@ -0,0 +1,11 @@
+﻿TimeSpan t1 = new TimeSpan(2, 2, 14, 49, 1);
+Console.WriteLine(t1.Days);
+
+var t2 = TimeSpan.FromMinutes(-5);
+var t3 = DateTime.Now.TimeOfDay;
+Console.WriteLine(t2);
+Console.WriteLine(t3.Minutes);
+
+var t4 = t3 - t2;
+
+Console.ReadLine();
