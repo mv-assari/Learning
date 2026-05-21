3e160c2e5433577c1bdc42f3e7d0155c391dc924
Mva <m.vahidassari@gmail.com>
2026-05-21 16:56:23 +0330

    add file for explain

---
+++ C#/3-FrameworkFundamentals/3-StirngBuilder/StirngBuilder/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+Stopwatch stopwatch = Stopwatch.StartNew();
+stopwatch.Start();
+
+string result = "hi ,";
+result += "welcome to mva";
+
+for (int i = 0; i < 50000; i++)
+{
+    result += i;
+}
+
+stopwatch.Stop();
+Console.WriteLine(stopwatch.ElapsedMilliseconds);
+
+stopwatch.Reset();
+StringBuilder stringBuilder = new StringBuilder("hi ,");
+stringBuilder.Append("welcome to mva");
+
+for (int i = 0; i < 50000; i++)
+{
+    stringBuilder.Append(i);
+}
+stopwatch.Stop();
+Console.WriteLine(stopwatch.ElapsedMilliseconds);
+
+
+
+Console.ReadLine();
