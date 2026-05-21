2e025eb9b2fbf3d0baa996d73476bb62d66b384e
Mva <m.vahidassari@gmail.com>
2026-05-21 18:11:43 +0330

    add file for explain

---
+++ C#/3-FrameworkFundamentals/6-DateTime&DateTimeOffset/DateTime&DateTimeOffset/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+DateTime dt =new DateTime(1405,02,31,new PersianCalendar());
+DateTimeOffset dtOffset = DateTimeOffset.Now;
+Console.WriteLine(dt);
+Console.WriteLine(dtOffset);
+
+dt.AddDays(1);
+dt.AddYears(1);
+dt.AddMonths(1);
+dt.AddSeconds(1);
+dt.AddMinutes(1);
+dt.AddHours(1);
+
+
+//DateTime and DateTimeOffset 
+//خصوصیات مشترکی دارند
+
+Console.ReadLine();
