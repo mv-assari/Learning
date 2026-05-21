7bd03c694b2e99fd6885b2b6faa0ae3bcc93c249
Mva <m.vahidassari@gmail.com>
2026-05-21 16:31:16 +0330

    add file for explain

---
+++ C#/3-FrameworkFundamentals/2-StirngCompare/StirngCompare/Program.cs
@@ -0,0 +1,38 @@
+﻿string[] myList = { "A", "b", "D", "C", "d", "c" };
+
+var CurrentCulture = myList.OrderBy(x=>x,StringComparer.CurrentCulture);
+
+Console.WriteLine("CurrentCulture");
+foreach(var item in CurrentCulture)
+{
+    Console.WriteLine(item);
+
+}
+
+var InvariantCulture = myList.OrderBy(x => x, StringComparer.InvariantCulture);
+
+Console.WriteLine("InvariantCulture");
+foreach (var item in InvariantCulture)
+{
+    Console.WriteLine(item);
+
+}
+
+var Ordinal = myList.OrderBy(x => x, StringComparer.Ordinal);
+
+Console.WriteLine("Ordinal");
+foreach (var item in Ordinal)
+{
+    Console.WriteLine(item);
+
+}
+
+Console.WriteLine("Mva" == "mva");
+Console.WriteLine("Mva".Equals("mva",StringComparison.OrdinalIgnoreCase));
+
+
+Console.WriteLine("a".CompareTo("A"));
+Console.WriteLine("a".CompareTo("a"));
+Console.WriteLine("A".CompareTo("a"));
+
+Console.ReadLine();
