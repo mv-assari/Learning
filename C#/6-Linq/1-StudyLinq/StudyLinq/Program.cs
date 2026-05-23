11634276fd588095e07c96ec7f45d80f03edda7f
Mva <m.vahidassari@gmail.com>
2026-05-23 21:51:44 +0330

    add file for explain

---
+++ C#/6-Linq/1-StudyLinq/StudyLinq/Program.cs
@@ -0,0 +1,10 @@
+﻿string[] names = { "Parsa", "Arash", "Vahid", "Fatemeh" };
+
+var result = names.Where(x => x.StartsWith('V')).ToList();
+
+foreach (var name in result)
+{
+    Console.WriteLine(name);
+}
+
+Console.ReadLine();
