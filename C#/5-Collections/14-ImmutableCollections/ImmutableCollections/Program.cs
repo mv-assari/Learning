54d2e6e70d1582188eb9e59349ee7f6a0acccd09
Mva <m.vahidassari@gmail.com>
2026-05-23 18:31:55 +0330

    add file for explain

---
+++ C#/5-Collections/14-ImmutableCollections/ImmutableCollections/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Immutable;
+
+//ImmutableArray<T>;
+//ImmutableList<T>;
+//ImmutableDictionary<K,V>;
+//ImmutableHashSet<T>;
+//ImmutableSortedDictionary<K, V>;
+//ImmutableSortedSet<T>;
+//ImmutableQueue<T>;
+//ImmutableStack<T>;
+
+ImmutableList<int> mylist = ImmutableList.Create<int>(1,2,3,4);
+
+var newmylist= mylist.Add(5);
+
+//mylist.AddRange(newmylist);
+//mylist.RemoveRange(newmylist);
+
+//ImmutableList<int>.Builder listbuilder=ImmutableList.CreateBuilder<int>();
+
+
+ImmutableArray<int>.Builder builder= ImmutableArray.CreateBuilder<int>();
+
+builder.Add(1);
+builder.Add(2);
+builder.Add(3);
+builder.Add(4);
+builder.Add(5);
+
+var arraybuilder=builder.ToImmutable();
+
+foreach (var item in mylist)
+{
+    Console.WriteLine(item);
+}
+
+foreach (var item in newmylist)
+{
+    Console.WriteLine(item);
+}
+
+Console.ReadLine();
