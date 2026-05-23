4c9f38043ad594983d53495681c40f6373aad3ba
Mva <m.vahidassari@gmail.com>
2026-05-23 17:38:25 +0330

    add file for explain

---
+++ C#/5-Collections/8-Stack/Stack/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+
+Stack<int> st=new Stack<int>();
+
+st.Push(0);
+st.Push(1);
+st.Push(2);
+st.Push(3);
+st.Push(4);
+
+Console.WriteLine(st.Peek());
+Console.WriteLine(st.Count);
+Console.WriteLine(st.Pop());
+
+st.Contains(0);
+st.Clear();
+
+//nonGeneric
+
+Stack ng=new Stack();
+//we can use object to input data
