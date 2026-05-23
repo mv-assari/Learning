7ab8878b615aeeda0e305b57f1d1dc512fa987f9
Mva <m.vahidassari@gmail.com>
2026-05-23 17:43:09 +0330

    add file for explain

---
+++ C#/5-Collections/9-BitArray/BitArray/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+
+var bits = new BitArray(3);
+
+bits.Set(0, true);
+bits.Set(1, true);
+bits.Set(2, true);
+
+Console.WriteLine(bits.Get(0));
+bits[2] = false;
+bits.SetAll(true);
