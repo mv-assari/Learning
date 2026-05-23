2e726a53adaed18e4e21105893b7583442919b4a
Mva <m.vahidassari@gmail.com>
2026-05-23 18:16:10 +0330

    add file for explain

---
+++ C#/5-Collections/13-HashTable/HashTable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+Hashtable ht= new Hashtable();
+
+ht.Add(1, "a");
+ht.Add(2, "b");
+ht.Add(3, "c");
+ht.Add(4, "d");
+
+foreach (DictionaryEntry item in ht)
+{
+    Console.WriteLine($"key:{item.Key} value:{item.Value}");
+}
+var key1 = ht[2];
+
+ht.Remove(1);
+ht.Clear();
+
+ht.Contains(1);
+ht.ContainsKey(2);
+ht.ContainsValue("b");
+
+
+Console.ReadLine();
