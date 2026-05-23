677506f87171a8c4708754af18fe41b6071ef154
Mva <m.vahidassari@gmail.com>
2026-05-23 14:26:01 +0330

    add file for explain

---
+++ C#/5-Collections/2-Enumeration/Enumeration/Program.cs
@@ -0,0 +1,30 @@
+﻿//public interface IEnumerator
+//{
+//    bool NextValue();
+//    object Current {  get; }
+//    void Reset();
+//}
+
+//public interface IEnumerable
+//{
+//    IEnumerator GetEnumerator();
+//}
+
+
+using System.Collections;
+
+string str = "mva.com";
+
+IEnumerator enumerator=str.GetEnumerator();
+
+while (enumerator.MoveNext())
+{
+    Console.WriteLine(enumerator.Current);
+}
+
+foreach (var item in str)
+{
+    Console.WriteLine(item);
+}
+
+Console.ReadLine();
