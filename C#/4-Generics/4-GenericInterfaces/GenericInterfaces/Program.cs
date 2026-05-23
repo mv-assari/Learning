b4ecd5b4776512fefcb4ec17d9cc8efce179099b
Mva <m.vahidassari@gmail.com>
2026-05-23 13:00:38 +0330

    add file for explain

---
+++ C#/4-Generics/4-GenericInterfaces/GenericInterfaces/Program.cs
@@ -0,0 +1,51 @@
+﻿
+Test test = new Test();
+
+test.Add(2);
+test.Add(3);
+test.Add(4);
+test.Add(5);
+test.Add(6);
+test.Add(7);
+
+var a=test.GetList();
+
+
+Console.ReadLine();
+
+public interface ImyInterface<T>
+{
+    List<T> GetList();
+}
+
+public class Test:ImyInterface<int>
+{
+    List<int> _list=new List<int>();
+
+    public void Add(int item)
+    {
+        _list.Add(item);
+    }
+
+    public List<int> GetList()
+    {
+        return _list;
+    }
+}
+
+
+public class MyClass<T> : ImyInterface<T>
+{
+    List<T> _list=new List<T>();
+
+    public void Add(T item)
+    {
+        _list.Add(item);
+    }
+
+
+    public List<T> GetList()
+    {
+        return _list;
+    }
+}
