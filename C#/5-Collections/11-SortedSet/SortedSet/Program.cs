a34255df6bf87d83d32e3fe923c87da48331bdb3
Mva <m.vahidassari@gmail.com>
2026-05-23 17:55:28 +0330

    add file for explain

---
+++ C#/5-Collections/11-SortedSet/SortedSet/Program.cs
@@ -0,0 +1,22 @@
+﻿SortedSet<int> st = new SortedSet<int>();
+
+//مقادیر تکراری قبول نمیکنه
+st.Add(1);
+st.Add(3);
+st.Add(2);
+st.Add(4);
+st.Add(6);
+st.Add(5);
+st.Add(8);
+st.Add(7);
+
+foreach (int i in st)
+{
+    Console.WriteLine(i);
+}
+
+st.Remove(1);
+st.RemoveWhere(x => x == 2);
+st.Clear();
+
+Console.ReadLine();
