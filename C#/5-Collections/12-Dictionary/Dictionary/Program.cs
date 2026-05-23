67c0fa2d58644b4aaebe12c87dee329789f67ae1
Mva <m.vahidassari@gmail.com>
2026-05-23 18:08:14 +0330

    add file for explain

---
+++ C#/5-Collections/12-Dictionary/Dictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿IDictionary<int,string> dic = new Dictionary<int,string>();
+
+dic.Add(1, "a");
+dic.Add(2, "b");
+dic.Add(3, "c");
+dic.Add(4, "d");
+dic.Add(5, "e");
+dic.TryAdd(5, "f");
+
+dic.Remove(3);
+dic.Clear();
+dic.TryGetValue(2, out string s);
+dic.ContainsKey(2);
+
+var tryget = dic.TryGetValue(2, out s);
+
+
+
+Console.WriteLine(s);
