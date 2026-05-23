e89a829ee610fa8fb3c8d13168500758a6882fe7
Mva <m.vahidassari@gmail.com>
2026-05-23 17:19:41 +0330

    add file for explain

---
+++ C#/5-Collections/6-LinkedList/LinkedList/Program.cs
@@ -0,0 +1,27 @@
+﻿LinkedList<int> linked=new LinkedList<int>();
+
+linked.AddFirst(1);
+linked.AddLast(2);
+linked.AddBefore(linked.Last, 3);
+linked.AddAfter(linked.Last, 4);
+
+foreach (var item in linked)
+{
+    Console.WriteLine(item);
+}
+
+linked.Clear();
+linked.RemoveFirst();
+linked.RemoveLast();
+linked.Remove(2);
+linked.Remove(linked.First);
+
+linked.Contains(3);
+linked.Find(2);
+linked.FindLast(2);
+
+//linked.CopyTo(/*array*/,/*index*/);
+
+
+
+Console.ReadLine();
