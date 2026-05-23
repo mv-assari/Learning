bc07357cb34a63f7dde528a4598d006ca91f3cc9
Mva <m.vahidassari@gmail.com>
2026-05-23 17:50:04 +0330

    add file for explain

---
+++ C#/5-Collections/10-HashSet/HashSet/Program.cs
@@ -0,0 +1,21 @@
+﻿HashSet<int> hs = new HashSet<int>();
+
+hs.Add(0);
+hs.Add(1);
+hs.Add(2);
+hs.Add(3);
+hs.Add(4);
+hs.Add(5);
+
+foreach (var item in hs)
+{
+    Console.WriteLine(item);
+}
+
+hs.Remove(0);
+hs.RemoveWhere(x=>x>2);
+hs.Clear();
+
+
+
+Console.ReadLine();
