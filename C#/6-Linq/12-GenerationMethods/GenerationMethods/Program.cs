8a5f7f6f8062c29212e5a0abf1f66ae2f0773a16
Mva <m.vahidassari@gmail.com>
2026-05-24 07:56:19 +0330

    add file for explain

---
+++ C#/6-Linq/12-GenerationMethods/GenerationMethods/Program.cs
@@ -0,0 +1,29 @@
+﻿
+var result=Product.GetAllProdcut().DefaultIfEmpty(new Product
+{
+    Id=1,
+    Name="p1",
+    Price=10000
+});
+
+
+var collection = Enumerable.Empty<string>();
+
+var range = Enumerable.Range(10, 10);
+
+var repeat=Enumerable.Repeat(100, 5);
+
+
+//-----------------------------------
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Price { get; set; }
+
+    public static List<Product> GetAllProdcut()
+    {
+        return new List<Product>();
+    }
+}
