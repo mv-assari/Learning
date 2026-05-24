61d9ce79c65e9a75f15ddd230620da12f5bc9ca6
Mva <m.vahidassari@gmail.com>
2026-05-24 06:48:33 +0330

    add file for explain

---
+++ C#/6-Linq/8-Grouping/Grouping/Program.cs
@@ -0,0 +1,29 @@
+﻿List<string> files = new List<string>()
+{
+    "a.exe",
+    "b.exe",
+    "c.pdf",
+    "d.pdf",
+    "e.pdf",
+    "f.config",
+    "g.dll",
+    "h.dll",
+    "i.docx",
+};
+
+
+var resutl2=files.ToLookup(f=>Path.GetExtension(f));//imediate
+
+
+var resutl=files.GroupBy(f=>Path.GetExtension(f));//defferd
+
+foreach(var file in resutl)
+{
+    Console.WriteLine($"File Extention:{file.Key}");
+    foreach (var item in file)
+    {
+        Console.WriteLine($"      -{item}");
+    }
+}
+
+Console.ReadLine();
