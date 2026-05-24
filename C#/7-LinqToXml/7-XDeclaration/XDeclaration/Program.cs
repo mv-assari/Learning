fd2610881870c213dc29c73125e2ddc5513fb152
Mva <m.vahidassari@gmail.com>
2026-05-24 16:06:17 +0330

    add file for explain

---
+++ C#/7-LinqToXml/7-XDeclaration/XDeclaration/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Xml.Linq;
+
+
+XDeclaration declaration = new XDeclaration("1.0", "UTF-16", "no");
+XDocument document = new XDocument(declaration);
+XElement element = new XElement("Contacts"
+                    , new XComment("this is test comment")
+                    , new XElement("Contact"
+                    , new XAttribute("Id", 1001)
+                    , new XComment("comment text")
+                    , new XAttribute("City", "Tehran")));
+
+document.Add(element);
+
+//Console.WriteLine(document);
+
+document.Save(@"D:\file\docc.xml");
+
+
+
+//Console.ReadLine();
