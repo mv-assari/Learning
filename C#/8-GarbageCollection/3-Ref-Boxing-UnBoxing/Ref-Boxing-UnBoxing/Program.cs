b10fc5856a8fb0b0252096d6ccfd0567385e5321
Mva <m.vahidassari@gmail.com>
2026-05-24 16:55:24 +0330

    add file for explain

---
+++ C#/8-GarbageCollection/3-Ref-Boxing-UnBoxing/Ref-Boxing-UnBoxing/Program.cs
@@ -0,0 +1,13 @@
+﻿int number = 20;
+Print(ref number);//ref
+Console.WriteLine(number);
+
+object a = 1;
+int b=(int )a; //unboxing
+object c = b;  //boxing
+
+void Print(ref int n) //ref
+{
+    n++;
+    Console.WriteLine(n);
+}
