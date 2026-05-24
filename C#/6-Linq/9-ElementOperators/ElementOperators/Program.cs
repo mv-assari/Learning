f7d69efd573f60b8ca1d2156600f43bb38af8d60
Mva <m.vahidassari@gmail.com>
2026-05-24 06:57:47 +0330

    add file for explain

---
+++ C#/6-Linq/9-ElementOperators/ElementOperators/Program.cs
@@ -0,0 +1,28 @@
+﻿List<String> names = new List<String>()
+{
+ "babak",
+ "maysam",
+ "mona",
+ "ali",
+ "ebrahim",
+ "bita",
+ "behnam",
+ "elham",
+ "behnaz"
+};
+
+
+//var result = names.First();
+var result = names.First(p=>p.Contains('s'));
+var resultordefault = names.FirstOrDefault(p=>p.Contains('s'));
+
+//var result2 = names.Last();
+var result2 = names.Last(p=>p.StartsWith('x'));
+var result2ordefault = names.LastOrDefault(p => p.Contains('x'));
+
+var single = names.Single(p => p.Contains('s'));
+var singleordefault = names.SingleOrDefault(p => p.Contains('s'));
+
+var elementat = names.ElementAt(0);
+var elementatordefault = names.ElementAtOrDefault(10);
+
