b967303bffef310af6b6f084304d3bfecbd53808
Mva <m.vahidassari@gmail.com>
2026-05-24 08:09:32 +0330

    add file for explain

---
+++ C#/6-Linq/14-ConversionMethods/ConversionMethods/Program.cs
@@ -0,0 +1,13 @@
+﻿object[] array = {"one",2,3};
+
+var result = array.AsEnumerable();
+
+var oftyp = array.OfType<int>();
+
+var cast=array.Cast<int>();
+
+var tohashset = array.ToHashSet();
+
+var toarray = result.ToArray();
+
+var queryable = array.AsQueryable();
