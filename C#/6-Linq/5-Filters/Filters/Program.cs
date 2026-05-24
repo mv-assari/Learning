e8a2da0bbda7da7af7d6a34050a77cc27bc48026
Mva <m.vahidassari@gmail.com>
2026-05-24 05:46:31 +0330

    add file for explain

---
+++ C#/6-Linq/5-Filters/Filters/Program.cs
@@ -0,0 +1,15 @@
+﻿string[] names = { "vahid", "fatemeh", "ali", "poneh", "popak", "sima", "nima" };
+
+var filter1=names.Where(n=>n.Contains('a')).ToList();
+
+var filter2=names.Where((n,i)=>n.Contains('a')&& i%2==0).ToList();
+
+var filter3 = names.Take(3).ToList();
+
+var filter4 = names.Skip(3).ToList();
+
+var filter5 = names.TakeWhile(n => n.Contains('a')).ToList();
+
+var filter6= names.SkipWhile(n=>n.Contains('b')).ToList();
+
+var filter7= names.Distinct().ToList();
