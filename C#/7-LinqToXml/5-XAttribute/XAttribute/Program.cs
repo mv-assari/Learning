6a3c2ce89ef900bb83365761de6e730bf5c2b4ad
Mva <m.vahidassari@gmail.com>
2026-05-24 15:44:55 +0330

    add file for explain

---
+++ C#/7-LinqToXml/5-XAttribute/XAttribute/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Xml.Linq;
+
+XElement element = new XElement("Contacts", new XElement("Contact", new XAttribute("Id", 1001), new XAttribute("City", "Tehran")));
+
+
+foreach (var item in element.Elements().FirstOrDefault().Attributes())
+{
+    Console.WriteLine(item);
+}
+
+element.Elements().FirstOrDefault().Add(new XAttribute("Country", "Iran"));
+
+
+element.Elements().FirstOrDefault().Attribute("Id").Remove();
+
+element.Elements().FirstOrDefault().Attribute("City").SetValue("Qom");
+
+Console.WriteLine(element);
+
+
+Console.ReadLine();
