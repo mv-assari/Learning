af35ac029146eb8d6975a717da47a8ba20127ebe
Mva <m.vahidassari@gmail.com>
2026-05-24 05:20:14 +0330

    add file for explain

---
+++ C#/6-Linq/3-QueryExpressions/QueryExpressions/Program.cs
@@ -0,0 +1,25 @@
+﻿string[] names = {"vahid","arash","mahdi","yaser" };
+
+var query = from c in names
+            where c.Length >= 3
+            where c.Contains('a')
+            orderby c.Length
+          select c.ToUpper();
+
+foreach (var item in query)
+{
+    Console.WriteLine(item);
+}
+
+
+ var method=names.Where(c=>c.Length >= 3)
+                 .Where(c=>c.Contains('a'))
+                 .OrderBy(c=>c.Length)
+                 .Select(c=>c.ToUpper());
+
+foreach (var item in method)
+{
+    Console.WriteLine(item);
+}
+
+Console.ReadLine();
