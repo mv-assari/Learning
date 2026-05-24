4f9e028342e7bd81dd2eafad010f65b0fe717a5c
Mva <m.vahidassari@gmail.com>
2026-05-24 08:03:23 +0330

    add file for explain

---
+++ C#/6-Linq/13-SetOperators/SetOperators/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array1 = { 1, 2, 3, 4, 3 };
+int[] array2 = { 2, 3, 4, 5 };
+
+var except=array1.Except(array2);
+
+var distinct = array1.Distinct();
+
+var intersect=array1.Intersect(array2); //اشتراک
+
+var concat=array1.Concat(array2); //اتصال دو لیست
+
+var union=array1.Union(array2); //اجتماع بدون ایتم تکراری
+
