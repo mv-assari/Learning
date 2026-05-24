bd8f3274daa9e8a7eebf77acd5cb4b32aba85cf7
Mva <m.vahidassari@gmail.com>
2026-05-24 07:15:34 +0330

    add file for explain

---
+++ C#/6-Linq/11-Quantifiers/Quantifiers/Program.cs
@@ -0,0 +1,27 @@
+﻿List<Product> products = new List<Product>()
+{
+    new Product{Id=1,Name="p1",Price=100000},
+    new Product{Id=2,Name="p2",Price=200000},
+    new Product{Id=3,Name="p3",Price=300000},
+    new Product{Id=4,Name="p4",Price=400000},
+    new Product{Id=5,Name="p5",Price=500000},
+};
+
+
+
+bool resultall = products.All(p=>p.Price>40000);
+
+bool resultany = products.Any(p => p.Price > 30000);
+
+var resultcontain = products.Where(p => p.Name.Contains('p'));
+
+bool sequenceEqual=list1.SequenceEqual(list2);
+
+//-------------------------
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Price { get; set; }
+}
