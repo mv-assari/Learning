a7831653d71641a7c462ce453a01a92053305e27
Mva <m.vahidassari@gmail.com>
2026-05-24 13:55:10 +0330

    add file for explain

---
+++ C#/7-LinqToXml/3-XDocument/XDocument/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Xml.Linq;
+
+XDocument doc1 = new XDocument();
+
+doc1.Add(new XElement("Curses", "test"));
+Console.WriteLine(doc1);
+//-------------------------------
+
+XDocument doc2 = XDocument.Load(@"F:\Learning\Learning\C#\7-LinqToXml\3-XDocument\XDocument\Product.xml");
+Console.WriteLine(doc2);
+
+//-------------------------------
+
+string xmldata = @"<product id=""101"" category=""electronics"">
+<name>لپ‌تاپ ایسوس</name>
+<brand>Asus</brand>
+<price currency=""IRR"">45,000,000</price>
+<inStock>true</inStock>
+<specs>
+<ram>16GB</ram>
+<storage>512GB SSD</storage>
+</specs>
+</product>";
+
+XDocument doc3 = XDocument.Parse(xmldata);
+Console.WriteLine(doc3);
+doc3.Save("xmlfile.xml");
+
+
+
+Console.ReadLine();
