e4932602144226c9e63436f723e03ed5e0199623
Mva <m.vahidassari@gmail.com>
2026-05-24 05:30:03 +0330

    add file for explain

---
+++ C#/6-Linq/4-Deferred&Immediate/Deferred&Immediate/Program.cs
@@ -0,0 +1,23 @@
+﻿List<int> numbers = new List<int>() { 10, 20, 30, 40 };
+
+var query = numbers.Where(n => n > 20);
+query = query.OrderByDescending(n => n);
+
+numbers.Add(50);
+
+foreach (var item in query)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("-----------------");
+numbers.Clear();
+
+foreach (var item in query)
+{
+    Console.WriteLine(item);
+}
+
+
+
+Console.ReadLine();
