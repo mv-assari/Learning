e7a60314d39f1169868e50cf6b115900caab1d11
Mva <m.vahidassari@gmail.com>
2026-05-25 16:38:20 +0330

    add file for explain

---
+++ C#/8-GarbageCollection/6-Finalize&Dispose/Finalize&Dispose/Program.cs
@@ -0,0 +1,48 @@
+﻿
+//Method(); // به صورت متد استفاده کنیم که به محض از بین رفتن رفرنس خودش به دنبال حذف شی ایجاد شده باشد
+//GC.Collect(); //این حالت توصیه نمشود چون سربار دارد مگر در حالت های بسیار خاص
+
+//TestClass test = new TestClass(); //به این صورت میتوان استفاده کرد
+//test.Run();
+//test.Dispose();
+
+using (TestClass test = new TestClass()) //یک روش دیگر
+{
+    test.Run();
+}
+
+
+
+Console.ReadLine();
+
+void Method()
+{
+    TestClass test=new TestClass();
+    test.Run();
+}
+
+//------------------------
+
+public class TestClass:IDisposable //هر کلاسی یا کتابخانه یا ... که اینترفیس  را پیاده سازی کرده میتوان این کار را انجام داد
+{
+    //DataBaseContext DataBase = new DataBaseContext();
+    public void Run()
+    {
+        Console.WriteLine("TestClass.Run()");
+    }
+
+    public void Dispose()
+    {
+        // DataBase.Dispose();
+        Console.WriteLine("testclass dispose()");
+    }
+
+    // finalize (~)
+    /// <summary>
+    /// این متد برای حذف شی از فضای رم میباشد
+    /// </summary>
+    ~TestClass()
+    {
+        Console.WriteLine("->> finalize Testclass");
+    }
+}
