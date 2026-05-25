fc81d65c46e9b56395a3e58d1641d8aa5ef7ad14
Mva <m.vahidassari@gmail.com>
2026-05-25 18:13:28 +0330

    add file for explain

---
+++ C#/8-GarbageCollection/10-WeakReference/WeakReference/Contact.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeakReference
+{
+    public class Contact
+    {
+        System.WeakReference Weak;
+        public Contact()
+        {
+            Weak = new System.WeakReference(null);
+        }
+
+        public List<string> GetContacts()
+        {
+            //چون از قبل دیتا رو گرفته بود ولی با اینکه کار متد و کلاس تموم بود و جی سی باید اونو از حافظه پاک میکرد ولی با این روش دیگه دوباره پردازش سنگین اتفاق نمی افته
+            var data=Weak.Target as List<string>; 
+            if(data is null)
+            {
+                Weak.Target =LoadData();
+                data=Weak.Target as List<string>;
+            }
+
+            return data;
+        }
+
+        public List<string> LoadData()
+        {
+            return new List<string>
+            {
+                "ali",
+                "vahid",
+                "fatemeh"
+            };
+        }
+
+    }
+}
---
+++ C#/8-GarbageCollection/10-WeakReference/WeakReference/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+
+//WeakReference weak = new WeakReference("my data",false);
+
+//Console.WriteLine(weak.IsAlive);
+//Console.WriteLine(weak.Target);
+
+using WeakReference;
+
+Contact contact = new Contact();
+
+var data1 = contact.GetContacts();
+var data2 = contact.GetContacts();
+var data3 = contact.GetContacts();
+
+Console.ReadLine();
