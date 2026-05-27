0aef790e6d4c44934ff6ea83e35656cef6e4afe1
Mva <m.vahidassari@gmail.com>
2026-05-27 14:46:12 +0330

    add file for explain

---
+++ Asp.Net-Core/4-Dapper/1-InitDapper&Create/DapperApp/Controllers/CustomerController.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Repository;
+using System.Collections.Generic;
+
+namespace DapperApp.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly ICustomerRepository _customerRepository;
+
+        public CustomerController(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+        // GET: CustomerController
+        public ActionResult Index()
+        
+        {
+            return View(_customerRepository.GetCustomers());
+
+
+            //_customerRepository.Add(new CustomerDto { FirstName="Fatemeh",LastName="Tabasi"});
+
+            //List<CustomerDto> customers = new List<CustomerDto>
+            //{
+            //    new CustomerDto{FirstName="ali",LastName="karimi"},
+            //    new CustomerDto{FirstName="ali",LastName="khameni"},
+            //    new CustomerDto{FirstName="hashem",LastName="babaie"}
+            //};
+            //_customerRepository.Add(customers);
+
+            //_customerRepository.Delete(4);
+
+            //_customerRepository.Update(new CustomerDto { Id = 1 ,FirstName="Vahid",LastName="Maleki"});
+
+            //List<CustomerDto> customers = new List<CustomerDto>
+            //{
+            //    new CustomerDto{Id=1,FirstName="Vahid",LastName="Maleki"},
+            //    new CustomerDto{Id=2,FirstName="Zahra",LastName="Nemidonamchi"}
+            //};
+            //_customerRepository.Update(customers);
+
+        }
+
+        // GET: CustomerController/Details/5
+        public ActionResult Details(long id)
+        {
+            return View(_customerRepository.GetCustomer(id));
+        }
+
+        // GET: CustomerController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CustomerController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CustomerDto customer)
+        {
+            try
+            {
+                _customerRepository.Add(customer);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CustomerController/Edit/5
+        public ActionResult Edit(long id)
+        {
+            return View(_customerRepository.GetCustomer(id));
+        }
+
+        // POST: CustomerController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(CustomerDto customer)
+        {
+            try
+            {
+                _customerRepository.Update(customer);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CustomerController/Delete/5
+        public ActionResult Delete(long id)
+        {
+            return View(_customerRepository.GetCustomer(id));
+        }
+
+        // POST: CustomerController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(CustomerDto customer)
+        {
+            try
+            {
+                _customerRepository.Delete(customer.Id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Asp.Net-Core/4-Dapper/1-InitDapper&Create/Repository/CustomerRepository.cs
@@ -0,0 +1,100 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public interface ICustomerRepository
+    {
+        public int Add(CustomerDto customer);
+        public int Add(List<CustomerDto> customers);
+        public int Delete(long id);
+        public int Delete(List<long> Ids);
+        public int Update(CustomerDto customer);
+        public int Update(List<CustomerDto> customers);
+        public List<CustomerDto> GetCustomers();
+        public CustomerDto GetCustomer(long id);
+    }
+
+    //تجربه 
+    //Microsoft.Data.SqlClient نصب کردم چون ورژن جدید هنوز استیبل نشده بود و سازگار با دپر نبود به خطا میخوردم که باید ورژن های قبلی رو نصب میکردم وقتی کتابخونه
+    public class CustomerRepository:ICustomerRepository
+    {
+        private readonly string connectionString= @"Server=.;Initial Catalog=DapperDb;Integrated Security=true;TrustServerCertificate=True;";
+
+        public int Add(CustomerDto customer)
+        {
+            string Sql = "INSERT INTO CUSTOMER (FirstName,LastName) VALUES (@FirstName,@LastName)";
+            var connection = new SqlConnection(connectionString);//SqlConnection نیاز به نصب کتابخونه داره
+            var result= connection.Execute(Sql,new {FirstName=customer.FirstName,LastName=customer.LastName});// چون از دپر استفاده میکنیم نیاز به نصب کتابخونه دپر داره
+            return result;
+        }
+
+        public int Add(List<CustomerDto> customers)
+        {
+            string Sql = "INSERT INTO CUSTOMER (FirstName,LastName) VALUES (@FirstName,@LastName)";
+            var connection = new SqlConnection(connectionString);
+            var result = connection.Execute(Sql, customers);
+            return result;
+        }
+
+        public int Delete(long id)
+        {
+            string Sql = "DELETE FROM CUSTOMER WHERE ID=@Id";
+            var connection=new SqlConnection(connectionString);
+            var result=connection.Execute(Sql,new {Id=id});
+            return result;
+        }
+
+        public int Delete(List<long> ids)
+        {
+            string Sql = "DELETE FORM CUSTOMER WHERE ID=@Id";
+            var connection=new SqlConnection(connectionString);
+            var resutl=connection.Execute(Sql, ids);
+            return resutl;
+        }
+
+        public CustomerDto GetCustomer(long id)
+        {
+            string Sql = "SELECT * FROM CUSTOMER WHERE ID=@Id";
+            var connection = new SqlConnection(connectionString); //SqlConnection نیاز به نصب کتابخونه داره
+            var result = connection.QueryFirstOrDefault<CustomerDto>(Sql, new { ID=id }); // چون از دپر استفاده میکنیم نیاز به نصب کتابخونه دپر داره
+            return result;
+        }
+
+        public List<CustomerDto> GetCustomers()
+        {
+            string Sql = "SELECT Id,FirstName,LastName FROM CUSTOMER";
+            var connection = new SqlConnection(connectionString);
+            var result = connection.Query<CustomerDto>(Sql);
+            return result.ToList();
+        }
+
+        public int Update(CustomerDto customer)
+        {
+            string Sql = "UPDATE CUSTOMER SET FirstName=@FirstName , LastName=@LastName WHERE ID=@Id";
+            var connection = new SqlConnection(connectionString);
+            var result = connection.Execute(Sql,customer);
+            return result;
+        }
+
+        public int Update(List<CustomerDto> customers)
+        {
+            string Sql = "UPDATE CUSTOMER SET FirstName=@FirstName , LastName=@LastName WHERE Id=@Id";
+            var connection = new SqlConnection(connectionString);
+            var result= connection.Execute(Sql,customers);
+            return result;
+        }
+    }
+
+    public class CustomerDto
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
