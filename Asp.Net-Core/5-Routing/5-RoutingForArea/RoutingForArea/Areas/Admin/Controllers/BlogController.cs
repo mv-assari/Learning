9146082efa13922c8852ecb72218d60b3b474005
Mva <m.vahidassari@gmail.com>
2026-05-31 19:44:35 +0330

    add file for explain

---
+++ Asp.Net-Core/5-Routing/5-RoutingForArea/RoutingForArea/Areas/Admin/Controllers/BlogController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Docs.Samples;
+
+namespace RoutingForArea.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BlogController : Controller
+    {
+        [HttpGet("[area]/[controller]/[action]",Name ="AdminRoute")]
+        public IActionResult Index()
+        {
+            return ControllerContext.MyDisplayRouteInfo();
+        }
+    }
+}
