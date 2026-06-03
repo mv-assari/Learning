e872a9685882577d2b7449fc62946d3283cb1153
Mva <m.vahidassari@gmail.com>
2026-06-03 22:28:01 +0330

    add file for explain

---
+++ Asp.Net-Core/6-ModelBinding/3-ComplexTypesBinding/ComplexTypesBinding/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using ComplexTypesBinding.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ComplexTypesBinding.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Edit()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Edit(UserDto user)
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Asp.Net-Core/6-ModelBinding/3-ComplexTypesBinding/ComplexTypesBinding/Models/UserDto.cs
@@ -0,0 +1,15 @@
+﻿namespace ComplexTypesBinding.Models
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public Info Info { get; set; }
+    }
+
+    public class Info
+    {
+        public string Company { get; set; }
+    }
+}
