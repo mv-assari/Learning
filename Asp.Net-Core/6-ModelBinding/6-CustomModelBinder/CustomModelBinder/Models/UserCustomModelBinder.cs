8010d104c95d6e91c57b4ad1f249b78aee84ef3e
Mva <m.vahidassari@gmail.com>
2026-06-03 23:45:24 +0330

    add file for explain

---
+++ Asp.Net-Core/6-ModelBinding/6-CustomModelBinder/CustomModelBinder/Models/UserCustomModelBinder.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Threading.Tasks;
+
+namespace CustomModelBinder.Models
+{
+    //1|vahid|mva@gmail.com
+    public class UserCustomModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            if(bindingContext==null)
+                throw new ArgumentNullException(nameof(bindingContext));
+
+            var values = bindingContext.ValueProvider.GetValue("user");
+
+            if (values.Length==0)
+            {
+                return Task.CompletedTask;
+            }
+
+            var splitData = values.FirstValue.Split('|');
+            if (splitData.Length >= 0)
+            {
+                User user = new User
+                {
+                    Id = int.Parse(splitData[0]),
+                    Name = splitData[1],
+                    Email = splitData[2]
+                };
+
+                bindingContext.Result = ModelBindingResult.Success(user);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
