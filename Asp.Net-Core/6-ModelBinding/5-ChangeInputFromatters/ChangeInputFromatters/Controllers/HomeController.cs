506c9ba1841b8625fa0d09892b6f56208a7b0d31
Mva <m.vahidassari@gmail.com>
2026-06-03 23:19:50 +0330

    add file for explain

---
+++ Asp.Net-Core/6-ModelBinding/5-ChangeInputFromatters/ChangeInputFromatters/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using ChangeInputFromatters.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ChangeInputFromatters.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        //تمام درخواست هایی که ما به سرور ارسال میکنیم به صورت پیشفرض با فرمت جیسون ارسال میشه
+        //میتونیم این رو تغییر بدیم
+        //میتونیم پکیج های مختلفی برای انجام این کار انجام بدیم
+        //[Consumes("application/json")]
+        //[Consumes("application/xml")]
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
