5c23d17612ca163b584b43cbf2218c91b0364083
Mva <m.vahidassari@gmail.com>
2026-06-12 18:22:40 +0330

    add file for explain

---
+++ Asp.Net-Core/7-Filters/4-ResourceFilters/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using AuthorizationFilter.Models;
+using AuthorizationFilter.Models.Filters;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AuthorizationFilter.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        //[CacheResourceFilter] //چون در سازنده ورودی داریم پس از خصوصیت زیر استفاده میکنیم 
+        //البته در جلسات بعدی تزریق وابستگی در خصوصیات رو آموزش میدیم
+        [TypeFilter(typeof(CacheResourceFilter))]
+        public IActionResult Index()
+        {
+            return View("Index",$"this is a text and generated at:{DateTime.Now.TimeOfDay}");
+        }
+
+        [AuthorizeActionFilter("User")]
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        [AuthorizeActionFilter("Admin")]
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Asp.Net-Core/7-Filters/4-ResourceFilters/Models/Filters/CacheResourceFilter.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System.Collections.Generic;
+
+namespace AuthorizationFilter.Models.Filters
+{
+    public class CacheResourceFilter : IResourceFilter
+    {
+        private readonly IModelMetadataProvider _modelMetadataProvider;
+
+        public CacheResourceFilter(IModelMetadataProvider modelMetadataProvider)
+        {
+            _modelMetadataProvider = modelMetadataProvider;
+        }
+
+        private static readonly Dictionary<string, object> _cache = new Dictionary<string, object>();
+        private string _cacheKey;
+        public void OnResourceExecuting(ResourceExecutingContext context)
+        {
+            _cacheKey=context.HttpContext.Request.Path.ToString();
+
+            if (_cache.ContainsKey(_cacheKey))
+            {
+                var cachedData=_cache[_cacheKey] as string;
+                if (cachedData != null)
+                {
+                    context.Result = new ViewResult()
+                    {
+                        ViewName = "Index",
+                        ViewData = new ViewDataDictionary(_modelMetadataProvider, context.ModelState)
+                        {
+                            Model = cachedData
+                        }
+                    };
+                }
+            }
+        }
+
+        public void OnResourceExecuted(ResourceExecutedContext context)
+        {
+            if (!string.IsNullOrEmpty(_cacheKey) && !_cache.ContainsKey(_cacheKey))
+            {
+                var result = context.Result as ViewResult;
+                if (result != null)
+                {
+                    _cache.Add(_cacheKey, result.Model);
+                }
+            }
+        }
+
+    }
+}
