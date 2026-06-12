d00638b9466f26c78ee1bee4ecd4cb3d71dd2c67
Mva <m.vahidassari@gmail.com>
2026-06-12 17:47:50 +0330

    add file for explain

---
+++ Asp.Net-Core/7-Filters/3-AuthorizationFilter/AuthorizationFilter/Models/Filters/AuthorizeActionFilter.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Security.Claims;
+
+namespace AuthorizationFilter.Models.Filters
+{
+    public class AuthorizeActionFilter :Attribute, IAuthorizationFilter
+    {
+        private readonly string _role;
+
+        public AuthorizeActionFilter(string role)
+        {
+            _role = role;
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var isAuthorized=CheckUserPermision(context.HttpContext.User,_role);
+            if (!isAuthorized)
+            {
+                context.Result = new UnauthorizedResult();
+            }
+        }
+
+        private bool CheckUserPermision(ClaimsPrincipal user,string role)
+        {
+            if (role == "Admin")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
