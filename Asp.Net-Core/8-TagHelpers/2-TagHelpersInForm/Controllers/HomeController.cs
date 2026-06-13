64dacc4fe94e94add8756f0e91fc325aedc9c1b1
Mva <m.vahidassari@gmail.com>
2026-06-13 22:23:17 +0330

    add file for explain

---
+++ Asp.Net-Core/8-TagHelpers/2-TagHelpersInForm/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using InitTagHelpers.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace InitTagHelpers.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Home/Index",Name ="indexRoute")]
+        [HttpPost]
+        public IActionResult Index(User user)
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Asp.Net-Core/8-TagHelpers/2-TagHelpersInForm/Models/User.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace InitTagHelpers.Models
+{
+    public class User
+    {
+        [DisplayName("نام")]
+        public string FirstName { get; set; }
+
+        [DisplayName("نام خانوادگی")]
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        public bool Gender { get; set; }
+
+        [Range(12,100)]
+        public byte Age { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Url)]
+        public string WebSite { get; set; }
+
+        public Address Address { get; set; }
+
+        [MinLength(10)]
+        [MaxLength(100)]
+        public string Description { get; set; }
+    }
+
+
+    public class Address
+    {
+        public string HomeAddress { get; set; }
+        public string WorkAddress { get; set; }
+    }
+}
