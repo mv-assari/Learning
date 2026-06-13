ff4296ead5d4df90d63f21d46f69ed5436aafc26
Mva <m.vahidassari@gmail.com>
2026-06-13 19:04:05 +0330

    add file for explain

---
+++ Asp.Net-Core/7-Filters/5-ActionFilters/Models/Filters/ValidateModelAttribute.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Linq;
+
+namespace AuthorizationFilter.Models.Filters
+{
+    public class ValidateModelAttribute :Attribute, IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext context)//قبل از اجرای اکشن این مورد اجرا میشود
+        {
+            var param = context.ActionArguments.SingleOrDefault();
+            if (param.Value==null)
+            {
+                context.Result = new BadRequestObjectResult("Model is null");
+            }
+
+            if (!context.ModelState.IsValid)
+            {
+                context.Result = new BadRequestObjectResult(context.ModelState);
+            }
+        }
+        public void OnActionExecuted(ActionExecutedContext context)//بعد از اجرای اکشن این مورد اجرا میشود
+        {
+           
+        }
+
+    }
+}
---
+++ Asp.Net-Core/7-Filters/5-ActionFilters/Models/ProductViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthorizationFilter.Models
+{
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+    }
+}
