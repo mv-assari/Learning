47b2c53b293780bafbce1364a12b76e3aecd3767
Mva <m.vahidassari@gmail.com>
2026-06-13 19:25:39 +0330

    add file for explain

---
+++ Asp.Net-Core/7-Filters/6-ExceptionFilters/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using AuthorizationFilter.Models;
+using AuthorizationFilter.Models.Filters;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AuthorizationFilter.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [TypeFilter(typeof(CustomExceptionFilter))]
+        public IActionResult Error()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
+    }
+}
---
+++ Asp.Net-Core/7-Filters/6-ExceptionFilters/Models/Filters/CustomExceptionFilter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System;
+
+namespace AuthorizationFilter.Models.Filters
+{
+    public class CustomExceptionFilter : Attribute, IExceptionFilter
+    {
+        private readonly IModelMetadataProvider _modelMetadataProvider;
+
+        public CustomExceptionFilter(IModelMetadataProvider modelMetadataProvider)
+        {
+            _modelMetadataProvider = modelMetadataProvider;
+        }
+
+        public void OnException(ExceptionContext context)
+        {
+            var result = new ViewResult { ViewName = "CustomException" };
+            result.ViewData = new ViewDataDictionary(_modelMetadataProvider, context.ModelState);
+            result.ViewData.Add("Exception",context.Exception);
+            context.Result = result;
+        }
+    }
+}
