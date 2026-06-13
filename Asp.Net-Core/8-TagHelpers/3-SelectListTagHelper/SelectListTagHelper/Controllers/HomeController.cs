b55179249e61bd169fff77251bd301ff215ce8f7
Mva <m.vahidassari@gmail.com>
2026-06-13 23:00:37 +0330

    add file for explain

---
+++ Asp.Net-Core/8-TagHelpers/3-SelectListTagHelper/SelectListTagHelper/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
+using SelectListTagHelper.Models;
+
+namespace SelectListTagHelper.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+            var tehranGroup = new SelectListGroup { Name = "tehran teams" };
+            var otherGroup = new SelectListGroup { Name = "other teams" };
+
+            TeamViewModel teamViewModel = new TeamViewModel
+            {
+                Teams = new List<SelectListItem>
+                {
+                    new SelectListItem {Value="1",Text="perspolis" },
+                    new SelectListItem {Value="2",Text="esteghlal" },
+                    new SelectListItem {Value="3",Text="zobeahan" },
+                    new SelectListItem {Value="4",Text="sepahan" },
+                    new SelectListItem {Value="5",Text="teraktor" },
+                },
+                TeamsOptionGroup=new List<SelectListItem>
+                {
+                    new SelectListItem {Group=tehranGroup, Value="1",Text="perspolis" },
+                    new SelectListItem {Group=tehranGroup,Value="2",Text="esteghlal" },
+                    new SelectListItem {Group = otherGroup, Value="3",Text="zobeahan" },
+                    new SelectListItem {Group = otherGroup,Value="4",Text="sepahan" },
+                    new SelectListItem {Group = otherGroup,Value="5",Text="teraktor" },
+                }
+
+                
+            };
+
+            teamViewModel.Teams.Insert(0, new SelectListItem { Value = "", Text = "choose option" });
+            //teamViewModel.Team = "3";
+
+            return View(teamViewModel);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Asp.Net-Core/8-TagHelpers/3-SelectListTagHelper/SelectListTagHelper/Models/TeamViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SelectListTagHelper.Models
+{
+    public class TeamViewModel
+    {
+        public string Team { get; set; }
+        public List<SelectListItem> Teams { get; set; }
+        public List<SelectListItem> TeamsOptionGroup { get; set; }
+
+        public TeamEnum TeamEnum { get; set; }
+
+    }
+
+    public enum TeamEnum
+    {
+        [Display(Name = "پرسپولیس")]
+        perspolis,
+        esteghlal,
+        sepahan,
+        traktor
+    }
+}
