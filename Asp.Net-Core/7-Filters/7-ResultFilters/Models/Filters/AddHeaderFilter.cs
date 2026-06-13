4b04cef6dd447aa38c65c43fdaaf71a18b7843e2
Mva <m.vahidassari@gmail.com>
2026-06-13 19:34:05 +0330

    add file for explain

---
+++ Asp.Net-Core/7-Filters/7-ResultFilters/Models/Filters/AddHeaderFilter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+
+namespace AuthorizationFilter.Models.Filters
+{
+    public class AddHeaderFilter : Attribute, IResultFilter
+    {
+        public void OnResultExecuting(ResultExecutingContext context)
+        {
+            context.HttpContext.Response.Headers.Add("MyHeaderOnFilter", "this is a test add header");
+        }
+        public void OnResultExecuted(ResultExecutedContext context)
+        {
+
+        }
+
+    }
+}
