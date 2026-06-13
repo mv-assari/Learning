301589b7188fd685a6e34efbf803b6d9345d7ac8
Mva <m.vahidassari@gmail.com>
2026-06-13 21:16:33 +0330

    add file for explain

---
+++ Asp.Net-Core/7-Filters/8-DI&ScopeInFilters/DI&ScopeInFilters/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using DI_ScopeInFilters.Models;
+using DI_ScopeInFilters.Models.Filters;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DI_ScopeInFilters.Controllers
+{
+    //[ShowMessage("Controller")]
+    [TypeFilter(typeof(ShowMessage))]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        //[ShowMessage("Action")]
+        [TypeFilter(typeof(ShowMessage))] //در این قسمت میتوان فیلترهایی که نیاز به تزریق وابستگی دارند را استفاده کرد ولی سرویس لایف تایم را نمیتوانیم مدیریت کنیم
+        [ServiceFilter(typeof(ShowMessage))]// با این مورد میتوان سرویس لایف تایم را مدیریت میکنیم
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Asp.Net-Core/7-Filters/8-DI&ScopeInFilters/DI&ScopeInFilters/Models/Filters/ShowMessage.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Text;
+
+namespace DI_ScopeInFilters.Models.Filters
+{
+    public class ShowMessage :Attribute, IResultFilter
+    {
+        private readonly string _source;
+        private readonly ILogger _logger;
+
+        public ShowMessage(string source, ILogger logger)
+        {
+            _source = source;
+            _logger = logger;
+        }
+
+        public void OnResultExecuting(ResultExecutingContext context)
+        {
+            PrintMessage(context, $"{_source} Executing");
+        }
+        public void OnResultExecuted(ResultExecutedContext context)
+        {
+            PrintMessage(context, $"{_source} -> Executed");
+        }
+
+        private void PrintMessage(FilterContext context, string message)
+        {
+            byte[] bytes = Encoding.ASCII.GetBytes($"<div> <h1>{message}</h1> </div>");
+            context.HttpContext.Response.Body.WriteAsync(bytes, 0, bytes.Length);
+        }
+
+    }
+}
