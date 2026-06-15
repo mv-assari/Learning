9fc8491dc625cbbdd49f2938efd6d3bf971f9803
Mva <m.vahidassari@gmail.com>
2026-06-15 22:42:10 +0330

    add file for explain

---
+++ Refactoring/2-Code Smells/1-Bloaters/1-Long Method/4-Preserve Whole Object/Preserve Whole Object/ReportGenerator_Before.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Preserve_Whole_Object
+{
+    public class ReportGenerator_Before
+    {
+        public string GenerateEmployeeReport(
+        string firstName,
+        string lastName,
+        string employeeCode,
+        string department,
+        decimal baseSalary,
+        decimal overtimePay,
+        decimal tax,
+        int workingDays,
+        string phone,
+        string email,
+        string address)
+        {
+            var fullName = $"{firstName} {lastName}";
+            var totalSalary = baseSalary + overtimePay - tax;
+            var dailyRate = totalSalary / workingDays;
+
+            var report = $"گزارش کارمند: {fullName}\n" +
+                         $"کد: {employeeCode} - بخش: {department}\n" +
+                         $"حقوق پایه: {baseSalary}, اضافه‌کار: {overtimePay}, مالیات: {tax}\n" +
+                         $"حقوق کل: {totalSalary}, نرخ روزانه: {dailyRate:F0}\n" +
+                         $"تلفن: {phone}, ایمیل: {email}\n" +
+                         $"آدرس: {address}";
+
+            return report;
+        }
+    }
+}
