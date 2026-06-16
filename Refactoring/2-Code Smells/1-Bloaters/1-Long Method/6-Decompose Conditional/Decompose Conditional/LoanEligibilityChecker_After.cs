78bfdb009059fa1287709cc670c178ed276a21a6
Mva <m.vahidassari@gmail.com>
2026-06-16 23:29:40 +0330

    add file for explain

---
+++ Refactoring/2-Code Smells/1-Bloaters/1-Long Method/6-Decompose Conditional/Decompose Conditional/LoanEligibilityChecker_After.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Decompose_Conditional
+{
+    public class LoanEligibilityChecker_After
+    {
+        public string CheckEligibility(Customer customer, LoanRequest loan)
+        {
+            if (IsCreditWorthy(customer))
+            {
+                if (IsLoanAmountReasonable(customer,loan))
+                {
+                    if (HasHighDebtRatio(customer))
+                    {
+                        return "رد: نسبت بدهی بالا";
+                    }
+                    return "تایید: وام قابل پرداخت است";
+                }
+                return "رد: مبلغ یا مدت وام نامناسب";
+            }
+            return "رد: شرایط اولیه احراز نشد";
+
+        }
+
+        public bool IsCreditWorthy(Customer customer)
+        {
+            return (customer.CreditScore > 650 && customer.AnnualIncome > 500000000) ||
+                (customer.IsEmployee && customer.YearsAtCurrentJob > 2 && customer.AnnualIncome > 300000000);
+        }
+        public bool IsLoanAmountReasonable(Customer customer,LoanRequest loan)
+        {
+            return loan.Amount < customer.AnnualIncome * 0.3m && loan.DurationMonths <= 60;
+        }
+        public bool HasHighDebtRatio(Customer customer)
+        {
+            return customer.HasActiveLoans && customer.TotalDebt > customer.AnnualIncome * 0.5m;
+        }
+    }
+
+    public class Customer
+    {
+        public int CreditScore { get; set; }
+        public decimal AnnualIncome { get; set; }
+        public bool IsEmployee { get; set; }
+        public int YearsAtCurrentJob { get; set; }
+        public bool HasActiveLoans { get; set; }
+        public decimal TotalDebt { get; set; }
+    }
+
+    public class LoanRequest
+    {
+        public decimal Amount { get; set; }
+        public int DurationMonths { get; set; }
+    }
+}
---
+++ Refactoring/2-Code Smells/1-Bloaters/1-Long Method/6-Decompose Conditional/Decompose Conditional/LoanEligibilityChecker_Before.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Decompose_Conditional
+{
+    public class LoanEligibilityChecker_Before
+    {
+        public string CheckEligibility(Customer customer, LoanRequest loan)
+        {
+            if ((customer.CreditScore > 650 && customer.AnnualIncome > 500000000) ||
+                (customer.IsEmployee && customer.YearsAtCurrentJob > 2 && customer.AnnualIncome > 300000000))
+            {
+                if (loan.Amount < customer.AnnualIncome * 0.3m && loan.DurationMonths <= 60)
+                {
+                    if (customer.HasActiveLoans && customer.TotalDebt > customer.AnnualIncome * 0.5m)
+                    {
+                        return "رد: نسبت بدهی بالا";
+                    }
+                    return "تایید: وام قابل پرداخت است";
+                }
+                return "رد: مبلغ یا مدت وام نامناسب";
+            }
+            return "رد: شرایط اولیه احراز نشد";
+        }
+    }
+
+    public class Customer
+    {
+        public int CreditScore { get; set; }
+        public decimal AnnualIncome { get; set; }
+        public bool IsEmployee { get; set; }
+        public int YearsAtCurrentJob { get; set; }
+        public bool HasActiveLoans { get; set; }
+        public decimal TotalDebt { get; set; }
+    }
+
+    public class LoanRequest
+    {
+        public decimal Amount { get; set; }
+        public int DurationMonths { get; set; }
+    }
+}
