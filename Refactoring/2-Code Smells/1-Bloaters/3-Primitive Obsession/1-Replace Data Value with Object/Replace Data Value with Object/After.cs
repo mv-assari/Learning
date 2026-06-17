dd0f0c38701204e966bda987da766a9950c90e2b
Mva <m.vahidassari@gmail.com>
2026-06-18 01:23:14 +0330

    add file for explain

---
+++ Refactoring/2-Code Smells/1-Bloaters/3-Primitive Obsession/1-Replace Data Value with Object/Replace Data Value with Object/After.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Replace_Data_Value_with_Object
+{
+    public class After {}
+
+    public class Employee
+    {
+        public Email Email { get; set; }
+        public Mony Salary { get; set; }
+    }
+
+    public class Email
+    {
+        public Email(string value)
+        {
+            if(string.IsNullOrEmpty(value) || !value.Contains("@"))
+                throw new Exception("ایمیل نامعتبر است");
+            Value = value;
+        }
+
+        public string Value { get; }
+
+        public override string ToString()
+        {
+            return Value;
+        }
+    }
+
+    public class Money
+    {
+        public Money(decimal salary)
+        {
+            if (salary<0)
+                throw new Exception("حقوق نمی‌تواند منفی باشد");
+            Salary = salary;
+        }
+
+        public decimal Salary { get; }
+
+        public override string ToString()
+        {
+            return string.Format("{0:N0} تومان",Salary);
+        }
+
+    }
+
+    public class PayrollService
+    {
+        public void SendSalarySlip(Employee emp)
+        {
+            Console.WriteLine($"فیش حقوقی به {emp.Email} ارسال شد: {emp.Salary}");
+        }
+    }
+}
---
+++ Refactoring/2-Code Smells/1-Bloaters/3-Primitive Obsession/1-Replace Data Value with Object/Replace Data Value with Object/Before.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Replace_Data_Value_with_Object
+{
+    public class Before { }
+
+    public class Employee
+    {
+        public string Email { get; set; }
+        public decimal Salary { get; set; }
+    }
+
+    public class PayrollService
+    {
+        public void SendSalarySlip(Employee emp)
+        {
+            // اعتبارسنجی پراکنده ایمیل
+            if (string.IsNullOrEmpty(emp.Email) || !emp.Email.Contains("@"))
+                throw new Exception("ایمیل نامعتبر است");
+
+            // اعتبارسنجی پراکنده حقوق
+            if (emp.Salary < 0)
+                throw new Exception("حقوق نمی‌تواند منفی باشد");
+
+            // فرمت نمایش حقوق
+            string formattedSalary = string.Format("{0:N0} تومان", emp.Salary);
+
+            Console.WriteLine($"فیش حقوقی به {emp.Email} ارسال شد: {formattedSalary}");
+        }
+    }
+}
