a6189513aa51c88c2a93a12d9d691205c58bc155
Mva <m.vahidassari@gmail.com>
2026-06-18 02:35:55 +0330

    add file for explain

---
+++ Refactoring/2-Code Smells/1-Bloaters/3-Primitive Obsession/4-Replace Type Code with Class/Replace Type Code with Class/After.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Replace_Type_Code_with_Class
+{
+    public class After { }
+    public class Ticket
+    {
+        public TicketType Type { get; set; }
+        public string Title { get; set; }
+
+        public decimal GetEstimateHours()
+        {
+            return Type.EstimateHours;
+        }
+    }
+    public class TicketType
+    {
+        
+        private TicketType(string name,decimal estimateHours)
+        {
+            Name = name;
+            EstimateHours = estimateHours;
+        }
+
+        public string Name { get; }
+        public decimal EstimateHours { get; }
+
+        public static readonly TicketType Bug = new("Bug",4);
+        public static readonly TicketType Feature = new("Feature", 20);
+        public static readonly TicketType Task = new("Task", 8);
+        public static readonly TicketType Improvment = new("Improvment", 4);
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+    }
+}
---
+++ Refactoring/2-Code Smells/1-Bloaters/3-Primitive Obsession/4-Replace Type Code with Class/Replace Type Code with Class/Before.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Replace_Type_Code_with_Class
+{
+    public class Before { }
+    public class Ticket
+    {
+        // کد نوع تیکت
+        public const int BUG = 0;
+        public const int FEATURE = 1;
+        public const int TASK = 2;
+        public const int IMPROVEMENT = 3;
+
+        public int Type { get; set; }
+        public string Title { get; set; }
+
+        public decimal GetEstimateHours()
+        {
+            switch (Type)
+            {
+                case BUG: return 4;
+                case FEATURE: return 20;
+                case TASK: return 8;
+                case IMPROVEMENT: return 12;
+                default: throw new Exception("نوع نامعتبر");
+            }
+        }
+    }
+}
