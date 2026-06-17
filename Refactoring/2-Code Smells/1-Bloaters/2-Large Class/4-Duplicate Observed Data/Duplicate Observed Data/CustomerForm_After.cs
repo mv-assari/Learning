a8e8f68f208616a52a6434b291af2cad74d63c22
Mva <m.vahidassari@gmail.com>
2026-06-18 00:02:37 +0330

    add file for explain

---
+++ Refactoring/2-Code Smells/1-Bloaters/2-Large Class/4-Duplicate Observed Data/Duplicate Observed Data/CustomerForm_After.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Reflection.Emit;
+
+namespace Duplicate_Observed_Data
+{
+    public class CustomerForm_After : Form
+    {
+        private TextBox nameTextBox;
+        private Label resultLabel;
+        private Customer _Customer;
+
+        public CustomerForm_After()
+        {
+            _Customer = new Customer();
+        }
+
+        private void SyncFromDomain()
+        {
+            nameTextBox.Text = _Customer.CustomerName;
+        }
+
+        private void SyncToDomain()
+        {
+            _Customer.CustomerName = nameTextBox.Text;
+        }
+
+        private void ButtonCalculate_Click(object sender, EventArgs e)
+        {
+            SyncToDomain();
+            decimal discount =_Customer.CalculateDiscount();
+            resultLabel.Text = $"تخفیف: {discount:P0}";
+        }
+    }
+
+    public class Customer
+    {
+        public string CustomerName { get; set; }
+
+        public decimal CalculateDiscount()
+        {
+            // این منطق تجاریه، ولی توی Form هست!
+            if (CustomerName.Length > 10)
+                return 0.1m;
+            return 0.05m;
+        }
+    }
+}
---
+++ Refactoring/2-Code Smells/1-Bloaters/2-Large Class/4-Duplicate Observed Data/Duplicate Observed Data/CustomerForm_Before.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection.Emit;
+
+namespace Duplicate_Observed_Data
+{
+    public class CustomerForm_Before : Form
+    {
+        private TextBox nameTextBox;
+        private Label resultLabel;
+
+        public string CustomerName
+        {
+            get => nameTextBox.Text;
+            set => nameTextBox.Text = value;
+        }
+
+        public decimal CalculateDiscount()
+        {
+            // این منطق تجاریه، ولی توی Form هست!
+            if (CustomerName.Length > 10)
+                return 0.1m;
+            return 0.05m;
+        }
+
+        private void ButtonCalculate_Click(object sender, EventArgs e)
+        {
+            decimal discount = CalculateDiscount();
+            resultLabel.Text = $"تخفیف: {discount:P0}";
+        }
+    }
+}
