637459faed59212ad607c718fc68f3f9d40092eb
Mva <m.vahidassari@gmail.com>
2026-06-19 01:14:53 +0330

    add file for explain

---
+++ Refactoring/2-Code Smells/1-Bloaters/3-Primitive Obsession/6-Replace Type Code with StateStrategy/Replace Type Code with StateStrategy/Before.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Replace_Type_Code_with_StateStrategy
+{
+    public class Before
+    {
+    }
+
+    public class Document
+    {
+        public const int DRAFT = 0;
+        public const int REVIEW = 1;
+        public const int PUBLISHED = 2;
+
+        public int State { get; set; } = DRAFT;
+
+        public string GetStateName()
+        {
+            switch (State)
+            {
+                case DRAFT: return "پیش‌نویس";
+                case REVIEW: return "در بازبینی";
+                case PUBLISHED: return "منتشر شده";
+                default: throw new Exception("نامعتبر");
+            }
+        }
+
+        public void Next()
+        {
+            switch (State)
+            {
+                case DRAFT: State = REVIEW; break;
+                case REVIEW: State = PUBLISHED; break;
+                case PUBLISHED: throw new Exception("منتشر شده تغییر نمی‌کنه");
+            }
+        }
+
+        public bool CanEdit()
+        {
+            switch (State)
+            {
+                case DRAFT: return true;
+                case REVIEW: return true;
+                case PUBLISHED: return false;
+                default: throw new Exception("نامعتبر");
+            }
+        }
+    }
+}
