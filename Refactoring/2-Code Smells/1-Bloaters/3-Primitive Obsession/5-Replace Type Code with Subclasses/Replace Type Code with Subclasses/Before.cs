4a5a50640cb5dffefb96a0acd8f6cf8e708f35ce
Mva <m.vahidassari@gmail.com>
2026-06-19 00:20:07 +0330

    add file for explain

---
+++ Refactoring/2-Code Smells/1-Bloaters/3-Primitive Obsession/5-Replace Type Code with Subclasses/Replace Type Code with Subclasses/Before.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Replace_Type_Code_with_Subclasses
+{
+    public class Before { }
+    public class Vehicle
+    {
+        public const int CAR = 0;
+        public const int TRUCK = 1;
+        public const int MOTORCYCLE = 2;
+
+        public int Type { get; set; }
+        public string Model { get; set; }
+
+        public decimal GetTollPrice()
+        {
+            switch (Type)
+            {
+                case CAR: return 5000;
+                case TRUCK: return 15000;
+                case MOTORCYCLE: return 2000;
+                default: throw new Exception("نوع نامعتبر");
+            }
+        }
+
+        public int GetMaxSpeed()
+        {
+            switch (Type)
+            {
+                case CAR: return 180;
+                case TRUCK: return 100;
+                case MOTORCYCLE: return 220;
+                default: throw new Exception("نوع نامعتبر");
+            }
+        }
+    }
+}
