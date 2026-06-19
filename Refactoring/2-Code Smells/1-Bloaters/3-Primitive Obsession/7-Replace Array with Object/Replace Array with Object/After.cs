95b51bde5daa89c61c0ce5e26d760907e89e0dfb
Mva <m.vahidassari@gmail.com>
2026-06-19 12:03:12 +0330

    add file for explain

---
+++ Refactoring/2-Code Smells/1-Bloaters/3-Primitive Obsession/7-Replace Array with Object/Replace Array with Object/After.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Replace_Array_with_Object
+{
+    public class After{}
+
+    public class EmployeeService
+    {
+        public void RegisterEmployee(Employee employee)
+        {
+
+            employee.Validate();
+
+            string fullName = employee.FirstName + " " + employee.LastName;
+            decimal familyAllowance = employee.Children * 500000;
+
+            Console.WriteLine($"کارمند {fullName} با حقوق {employee.BaseSalary} و حق اولاد {familyAllowance} ثبت شد");
+        }
+    }
+
+    public class Employee
+    {
+        private string _firstName;
+        private decimal _baseSalary;
+
+        public string FirstName 
+        {
+            get{ return _firstName;}
+            set{_firstName = value;}
+        }
+        public string LastName { get; set; }
+        public string NationalCode { get; set; }
+        public decimal BaseSalary 
+        {
+            get{ return _baseSalary;}
+            set{ _baseSalary = value;}
+        }
+        public byte Children { get; set; }    
+
+        public void Validate()
+        {
+            if (string.IsNullOrEmpty(FirstName))
+                throw new Exception("نام الزامیست");
+            if (BaseSalary < 0)
+                throw new Exception("حقوق نامعتبر");
+        }
+    }
+}
---
+++ Refactoring/2-Code Smells/1-Bloaters/3-Primitive Obsession/7-Replace Array with Object/Replace Array with Object/Before.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Replace_Array_with_Object
+{
+    public class Before
+    {
+    }
+
+    public class EmployeeService
+    {
+        public void RegisterEmployee(string[] data)
+        {
+            // data[0] = نام
+            // data[1] = نام خانوادگی
+            // data[2] = کد ملی
+            // data[3] = حقوق پایه
+            // data[4] = تعداد فرزندان
+
+            if (string.IsNullOrEmpty(data[0]))
+                throw new Exception("نام الزامیست");
+
+            if (data[3] == null || decimal.Parse(data[3]) < 0)
+                throw new Exception("حقوق نامعتبر");
+
+            string fullName = data[0] + " " + data[1];
+            decimal salary = decimal.Parse(data[3]);
+            int children = int.Parse(data[4]);
+            decimal familyAllowance = children * 500000;
+
+            Console.WriteLine($"کارمند {fullName} با حقوق {salary} و حق اولاد {familyAllowance} ثبت شد");
+        }
+    }
+}
