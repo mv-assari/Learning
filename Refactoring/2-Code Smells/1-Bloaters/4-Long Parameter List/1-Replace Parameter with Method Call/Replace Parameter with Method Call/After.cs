ff93558eda550e7f4041388517fb4316fe2c7a98
Mva <m.vahidassari@gmail.com>
2026-06-19 12:32:51 +0330

    add file for explain

---
+++ Refactoring/2-Code Smells/1-Bloaters/4-Long Parameter List/1-Replace Parameter with Method Call/Replace Parameter with Method Call/After.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Replace_Parameter_with_Method_Call
+{
+    public class After
+    {
+    }
+
+    public class ReportGenerator
+    {
+        public string GenerateReport(Company company)
+        {
+            return $"گزارش {company.Name}\n" +
+                   $"آدرس: {company.Address}\n" +
+                   $"تلفن: {company.Phone}\n" +
+                   $"تاریخ: {company.GetCurrentDate()}";
+        }
+    }
+
+    public class Company
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+
+        public string GetCurrentDate()
+        {
+            return DateTime.Now.ToString("yyyy/MM/dd");
+        }
+    }
+}
---
+++ Refactoring/2-Code Smells/1-Bloaters/4-Long Parameter List/1-Replace Parameter with Method Call/Replace Parameter with Method Call/Before.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Replace_Parameter_with_Method_Call
+{
+    public class Before
+    {
+    }
+
+    public class ReportGenerator
+    {
+        public string GenerateReport(string companyName, string companyAddress,
+                                      string companyPhone, string currentDate)
+        {
+            return $"گزارش {companyName}\n" +
+                   $"آدرس: {companyAddress}\n" +
+                   $"تلفن: {companyPhone}\n" +
+                   $"تاریخ: {currentDate}";
+        }
+    }
+
+    public class Company
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+
+        public string GetCurrentDate()
+        {
+            return DateTime.Now.ToString("yyyy/MM/dd");
+        }
+    }
+}
