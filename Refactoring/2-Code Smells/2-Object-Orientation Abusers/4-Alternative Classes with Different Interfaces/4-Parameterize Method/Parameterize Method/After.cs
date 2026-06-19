2791461952b9f23e21f98638707b4170ea877cd4
Mva <m.vahidassari@gmail.com>
2026-06-19 16:47:07 +0330

    add file for explain

---
+++ Refactoring/2-Code Smells/2-Object-Orientation Abusers/4-Alternative Classes with Different Interfaces/4-Parameterize Method/Parameterize Method/After.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Parameterize_Method
+{
+    public class After
+    {
+    }
+
+    public class TaxCalculator
+    {
+        public decimal CalculateTax(decimal amount,decimal rate)
+        {
+            return amount * rate;
+        }    
+    }
+
+}
---
+++ Refactoring/2-Code Smells/2-Object-Orientation Abusers/4-Alternative Classes with Different Interfaces/4-Parameterize Method/Parameterize Method/Before.cs
@@ -0,0 +1,27 @@
+﻿//using System;
+
+//namespace Parameterize_Method
+//{
+//    public class Before
+//    {
+//    }
+
+//    public class TaxCalculator
+//    {
+//        public decimal CalculateTehranTax(decimal amount)
+//        {
+//            return amount * 0.1m;
+//        }
+
+//        public decimal CalculateShirazTax(decimal amount)
+//        {
+//            return amount * 0.07m;
+//        }
+
+//        public decimal CalculateMashhadTax(decimal amount)
+//        {
+//            return amount * 0.05m;
+//        }
+//    }
+
+//}
