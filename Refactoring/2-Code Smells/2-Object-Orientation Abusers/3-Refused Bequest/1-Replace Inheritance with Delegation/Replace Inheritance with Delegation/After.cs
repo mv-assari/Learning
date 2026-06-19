bd80de102fc021498af4be1246dc8d08f8384ad5
Mva <m.vahidassari@gmail.com>
2026-06-19 15:50:57 +0330

    add file for explain

---
+++ Refactoring/2-Code Smells/2-Object-Orientation Abusers/3-Refused Bequest/1-Replace Inheritance with Delegation/Replace Inheritance with Delegation/After.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Replace_Inheritance_with_Delegation
+{
+    public class After
+    {
+    }
+
+
+    public class Dictionary<TKey, TValue>
+    {
+        private List<(TKey, TValue)> _entries = new();
+
+        public void Add(TKey key, TValue value)
+        {
+            _entries.Add((key, value));
+        }
+
+        public TValue Get(TKey key)
+        {
+            return _entries.First(e => e.Item1.Equals(key)).Item2;
+        }
+
+        public bool Contains(TKey key)
+        {
+            return _entries.Any(e => e.Item1.Equals(key));
+        }
+
+        public int Count => _entries.Count;
+
+        public void Clear()
+        {
+            _entries.Clear();
+        }
+    }
+
+    public class Cache<TKey, TValue> 
+    {
+        private Dictionary<TKey, TValue> _dictionary = new();
+        public TValue GetOrAdd(TKey key, Func<TValue> factory)
+        {
+            if (!_dictionary.Contains(key))
+                _dictionary.Add(key, factory());
+            return _dictionary.Get(key);
+        }
+    }
+}
---
+++ Refactoring/2-Code Smells/2-Object-Orientation Abusers/3-Refused Bequest/1-Replace Inheritance with Delegation/Replace Inheritance with Delegation/Before.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Replace_Inheritance_with_Delegation
+{
+    public class Before
+    {
+    }
+
+    // کلاس والد
+    public class Dictionary<TKey, TValue>
+    {
+        private List<(TKey, TValue)> _entries = new();
+
+        public void Add(TKey key, TValue value)
+        {
+            _entries.Add((key, value));
+        }
+
+        public TValue Get(TKey key)
+        {
+            return _entries.First(e => e.Item1.Equals(key)).Item2;
+        }
+
+        public bool Contains(TKey key)
+        {
+            return _entries.Any(e => e.Item1.Equals(key));
+        }
+
+        public int Count => _entries.Count;
+
+        public void Clear()
+        {
+            _entries.Clear();
+        }
+    }
+
+    // ❌ زیرکلاس فقط می‌خواد یه Cache ساده باشه
+    // ولی کلی متد اضافی از والد به ارث برده
+    public class Cache<TKey, TValue> : Dictionary<TKey, TValue>
+    {
+        public TValue GetOrAdd(TKey key, Func<TValue> factory)
+        {
+            if (!Contains(key))
+                Add(key, factory());
+            return Get(key);
+        }
+
+        // ولی Clear هم در دسترسه! نباید باشه
+        // Count هم هست
+        // میتونن اشتباهی Clear کنن و کل Cache رو پاک کنن!
+    }
+}
