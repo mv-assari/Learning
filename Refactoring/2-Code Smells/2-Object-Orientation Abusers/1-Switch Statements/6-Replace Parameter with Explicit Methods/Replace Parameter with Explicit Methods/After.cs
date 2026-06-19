e525cf736ff2d7142207a293333af3902b80cf56
Mva <m.vahidassari@gmail.com>
2026-06-19 14:33:33 +0330

    add file for explain

---
+++ Refactoring/2-Code Smells/2-Object-Orientation Abusers/1-Switch Statements/6-Replace Parameter with Explicit Methods/Replace Parameter with Explicit Methods/After.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Replace_Parameter_with_Explicit_Methods
+{
+    public class After
+    {
+    }
+
+    public class Calculator
+    {
+        
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        public int Divide(int a, int b)
+        {
+            if(b==0)
+                throw new DivideByZeroException();
+            return a / b;
+        }
+    }
+}
---
+++ Refactoring/2-Code Smells/2-Object-Orientation Abusers/1-Switch Statements/6-Replace Parameter with Explicit Methods/Replace Parameter with Explicit Methods/Before.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Replace_Parameter_with_Explicit_Methods
+{
+    public class Before
+    {
+    }
+
+    public class Calculator
+    {
+        public int DoOperation(string operation, int a, int b)
+        {
+            if (operation.Equals("add"))
+                return a + b;
+
+            if (operation.Equals("subtract"))
+                return a - b;
+
+            if (operation.Equals("multiply"))
+                return a * b;
+
+            if (operation.Equals("divide"))
+            {
+                if (b == 0)
+                    throw new DivideByZeroException();
+                return a / b;
+            }
+
+            throw new Exception("عملیات نامعتبر");
+        }
+    }
+}
