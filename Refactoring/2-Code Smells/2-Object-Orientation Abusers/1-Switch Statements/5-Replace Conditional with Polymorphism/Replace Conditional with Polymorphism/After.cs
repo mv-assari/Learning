d28791c0bf138cb1fc812fb4176e737e49fca524
Mva <m.vahidassari@gmail.com>
2026-06-19 14:22:43 +0330

    add file for explain

---
+++ Refactoring/2-Code Smells/2-Object-Orientation Abusers/1-Switch Statements/5-Replace Conditional with Polymorphism/Replace Conditional with Polymorphism/After.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Replace_Conditional_with_Polymorphism
+{
+    public class After
+    {
+        public void MethodTest()
+        {
+            List<Employee> employees = new()
+            {
+                new Regular { Name = "علی", BaseSalary = 10000000 },
+                new Manager { Name = "مریم", BaseSalary = 15000000 },
+                new Executive { Name = "حسین", BaseSalary = 20000000 }
+            };
+
+            foreach (var emp in employees)
+            {
+                Console.WriteLine($"{emp.Name}: حقوق {emp.CalculateSalary():N0}, مرخصی {emp.GetVacationDays()} روز");
+            }
+
+            // خروجی:
+            // علی: حقوق 10,000,000, مرخصی 15 روز
+            // مریم: حقوق 22,500,000, مرخصی 20 روز
+            // حسین: حقوق 45,000,000, مرخصی 30 روز
+        }
+    }
+
+    public abstract class Employee
+    {
+        public string Name { get; set; }
+        public decimal BaseSalary { get; set; }
+
+        public abstract decimal CalculateSalary();
+
+        public abstract int GetVacationDays();
+
+    }
+
+    public class Regular : Employee
+    {
+        public override decimal CalculateSalary()
+        {
+            return BaseSalary;
+        }
+        public override int GetVacationDays()
+        {
+            return 15;
+        }
+    }
+    public class Manager : Employee
+    {
+        public override decimal CalculateSalary()
+        {
+            return BaseSalary * 1.5;
+        }
+
+        public override int GetVacationDays()
+        {
+            return 20;
+        }
+    }
+    public class Executive : Employee
+    {
+        public override decimal CalculateSalary()
+        {
+            return BaseSalary * 2m + 5000000;
+        }
+
+        public override int GetVacationDays()
+        {
+            return 30;
+        }
+    }
+}
---
+++ Refactoring/2-Code Smells/2-Object-Orientation Abusers/1-Switch Statements/5-Replace Conditional with Polymorphism/Replace Conditional with Polymorphism/Before.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Replace_Conditional_with_Polymorphism
+{
+    public class Before
+    {
+    }
+
+    public class Employee
+    {
+        public const int REGULAR = 0;
+        public const int MANAGER = 1;
+        public const int EXECUTIVE = 2;
+
+        public int Type { get; set; }
+        public string Name { get; set; }
+        public decimal BaseSalary { get; set; }
+
+        public decimal CalculateSalary()
+        {
+            switch (Type)
+            {
+                case REGULAR: return BaseSalary;
+                case MANAGER: return BaseSalary * 1.5m;
+                case EXECUTIVE: return BaseSalary * 2m + 5000000;
+                default: throw new Exception("نوع نامعتبر");
+            }
+        }
+
+        public int GetVacationDays()
+        {
+            switch (Type)
+            {
+                case REGULAR: return 15;
+                case MANAGER: return 20;
+                case EXECUTIVE: return 30;
+                default: throw new Exception("نوع نامعتبر");
+            }
+        }
+    }
+}
