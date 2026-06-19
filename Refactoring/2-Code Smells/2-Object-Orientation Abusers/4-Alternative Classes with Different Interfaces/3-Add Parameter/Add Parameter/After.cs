e0bdc50a36ad722085a48190dec7570e7d8a9505
Mva <m.vahidassari@gmail.com>
2026-06-19 16:36:22 +0330

    add file for explain

---
+++ Refactoring/2-Code Smells/2-Object-Orientation Abusers/4-Alternative Classes with Different Interfaces/3-Add Parameter/Add Parameter/After.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Add_Parameter
+{
+    public class After
+    {
+        public void Method()
+        {
+            // استفاده:
+            var sender = new EmailSender();
+            sender.SendEmail("user@test.com", "سلام", "خوبی؟");
+
+            // حالا می‌خوایم اینم بشه:
+            // sender.SendEmail("user@test.com", "admin@test.com", "سلام", "خوبی؟");
+            // ولی فعلاً فقط ۳ تا پارامتر داره!
+        }
+    }
+
+    public class EmailSender
+    {
+
+        public void SendEmail(string to, string subject, string body, string cc=null)
+        {
+            Console.WriteLine($"To: {to}");
+            if(!string.IsNullOrEmpty(cc))
+                Console.WriteLine($"CC: {cc}");
+            Console.WriteLine($"Subject: {subject}");
+            Console.WriteLine($"Body: {body}");
+            Console.WriteLine("Email sent.");
+        }
+
+        [Obsolete("این متد استفاده نمیشود و منقضی شده است")]
+        public void SendEmail(string to, string subject, string body)
+        {
+            // فرض کن حالا نیاز داریم cc هم اضافه کنیم
+            Console.WriteLine($"To: {to}");
+            Console.WriteLine($"Subject: {subject}");
+            Console.WriteLine($"Body: {body}");
+            Console.WriteLine("Email sent.");
+        }
+    }
+}
---
+++ Refactoring/2-Code Smells/2-Object-Orientation Abusers/4-Alternative Classes with Different Interfaces/3-Add Parameter/Add Parameter/Before.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Add_Parameter
+{
+    public class Before
+    {
+    }
+
+    public class EmailSender
+    {
+        public void SendEmail(string to, string subject, string body)
+        {
+            // فرض کن حالا نیاز داریم cc هم اضافه کنیم
+            Console.WriteLine($"To: {to}");
+            Console.WriteLine($"Subject: {subject}");
+            Console.WriteLine($"Body: {body}");
+            Console.WriteLine("Email sent.");
+        }
+}
+
+// استفاده:
+var sender = new EmailSender();
+sender.SendEmail("user@test.com", "سلام", "خوبی؟");
+
+// حالا می‌خوایم اینم بشه:
+// sender.SendEmail("user@test.com", "admin@test.com", "سلام", "خوبی؟");
+// ولی فعلاً فقط ۳ تا پارامتر داره!
+}
