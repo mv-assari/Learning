3ce1228b30a5eb5c2cde80f16161de042e21bc12
Mva <m.vahidassari@gmail.com>
2026-06-19 15:59:14 +0330

    add file for explain

---
+++ Refactoring/2-Code Smells/2-Object-Orientation Abusers/3-Refused Bequest/2-Extract Superclass/Extract Superclass/After.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Extract_Superclass
+{
+    public class After
+    {
+    }
+
+    public abstract class Person
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Id { get; set; }
+
+        public string GetContactInfo()
+        {
+            return $"{Name} - {Email}";
+        }
+    }
+
+    public class Employee:Person
+    {
+        
+        public decimal Salary { get; set; }
+
+        
+
+        public decimal CalculateAnnualSalary()
+        {
+            return Salary * 12;
+        }
+    }
+
+    public class Customer:Person
+    {
+
+        public string CustomerType { get; set; }
+
+        public decimal GetDiscount()
+        {
+            return CustomerType == "VIP" ? 0.2m : 0.05m;
+        }
+    }
+}
