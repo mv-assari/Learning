126da6d64609582e2a1c185427fa4f446ff79602
Mva <m.vahidassari@gmail.com>
2026-06-21 00:12:28 +0330

    add file for explain

---
+++ Refactoring/2-Code Smells/3-Change Preventers/3-Parallel Inheritance Hierarchies/2-Move Field/Move Field/After.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Move_Field
+{
+    public class After
+    {
+    }
+
+    public class Customer
+    {
+        public string Name { get; set; }
+        
+    }
+
+    public class LoanCalculator
+    {
+        public decimal InterestRate { get; set; } = 0.05m; 
+        public Customer Customer { get; set; }
+
+        public decimal CalculateInterest(decimal amount)
+        {
+            return amount * InterestRate; // فقط اینجا استفاده میشه
+        }
+    }
+}
