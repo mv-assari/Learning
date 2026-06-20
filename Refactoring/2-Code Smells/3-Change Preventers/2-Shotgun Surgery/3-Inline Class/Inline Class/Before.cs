f7999e8bef4e689e249e4a428cc8ff9926c46249
Mva <m.vahidassari@gmail.com>
2026-06-21 00:01:45 +0330

    add file for explain

---
+++ Refactoring/2-Code Smells/3-Change Preventers/2-Shotgun Surgery/3-Inline Class/Inline Class/Before.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Inline_Class
+{
+    public class Before
+    {
+    }
+
+    // کلاس تقریباً خالی
+    public class Phone
+    {
+        public string Number { get; set; }
+        public string Type { get; set; } // Mobile, Home, Work
+    }
+
+    public class Contact
+    {
+        public string Name { get; set; }
+        public Phone Phone { get; set; }
+
+        public void DisplayContact()
+        {
+            Console.WriteLine($"{Name}: {Phone.Number} ({Phone.Type})");
+        }
+    }
+}
