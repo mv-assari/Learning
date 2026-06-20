ec6b75ba3c08066bc366c66b588f5d624439bac3
Mva <m.vahidassari@gmail.com>
2026-06-20 23:53:34 +0330

    add file for explain

---
+++ Refactoring/2-Code Smells/3-Change Preventers/2-Shotgun Surgery/2-Move Field/Move Field/Before.cs
@@ -0,0 +1,24 @@
+﻿//using System;
+
+//namespace Move_Field
+//{
+//    public class Before
+//    {
+//    }
+
+//    public class Customer
+//    {
+//        public string Name { get; set; }
+//        public decimal InterestRate { get; set; } = 0.05m; // این فیلد اینجا بی‌ربطه
+//    }
+
+//    public class LoanCalculator
+//    {
+//        public Customer Customer { get; set; }
+
+//        public decimal CalculateInterest(decimal amount)
+//        {
+//            return amount * Customer.InterestRate; // فقط اینجا استفاده میشه
+//        }
+//    }
+//}
