d9ebb859c948387175a29508828f5ea02800fe11
Mva <m.vahidassari@gmail.com>
2026-06-23 01:46:23 +0330

    add file for explain

---
+++ Refactoring/2-Code Smells/4-Dispensables/6-Speculative Generality/1-Inline Class/Inline Class/After.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Inline_Class
+{
+    public class After
+    {
+    }
+
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Number { get; set; }
+        public string Type { get; set; } // Mobile, Home, Work
+
+        public void DisplayContact()
+        {
+            Console.WriteLine($"{Name}: {Number} ({Type})");
+        }
+    }
+}
