d848577ef44bfcf1a22c7e11fe1027456ca8709f
Mva <m.vahidassari@gmail.com>
2026-06-28 19:04:14 +0330

    add file for explain

---
+++ Asp.Net-Core/9-RazorPages/2-ConfigRazorPages/ConfigRazorPages/Data/DataBaseContext.cs
@@ -0,0 +1,13 @@
+﻿using ConfigRazorPages.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConfigRazorPages.Data
+{
+    public class DataBaseContext:DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> option):base(option) {}
+        
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
