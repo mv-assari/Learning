c4c4b8da930f8e1e49e45f1b4943b3c1a432423d
Mva <m.vahidassari@gmail.com>
2026-07-01 00:12:49 +0330

    add file for explain

---
+++ Asp.Net-Core/9-RazorPages/5-HandlerinRazorPage/Pages/Index.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ConfigRazorPages.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+       public void OnPost()
+        {
+
+        }
+
+
+        //ادامه نام متد میشه هندلر اون که در سمت فرانت میشه ازاین طریق بین متدهای پست حالتهای مختلف داشت
+        public void OnPostSendComment()
+        {
+
+        }
+
+        public void OnPostSubscribe()
+        {
+
+        }
+
+        [NonHandler]
+        public void Function()
+        {
+
+        }
+
+        
+    }
+}
