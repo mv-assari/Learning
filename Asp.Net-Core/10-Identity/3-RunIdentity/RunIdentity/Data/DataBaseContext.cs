2c0ee4eda369661245758300b58339f45da98118
Mva <m.vahidassari@gmail.com>
2026-07-02 20:03:15 +0330

    add file for explain

---
+++ Asp.Net-Core/10-Identity/3-RunIdentity/RunIdentity/Data/DataBaseContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace RunIdentity.Data
+{
+    public class DataBaseContext:IdentityDbContext<IdentityUser>
+    {
+        //در اینجا ما چند کتابخانه باید نصب کنیم 
+        //Microsoft.AspNetCore.Identity.EntityFrameworkCore
+        //Microsoft.EntityFrameworkCore
+        //Microsoft.EntityFrameworkCore.Design
+        //Microsoft.EntityFrameworkCore.SqlServer
+        //Microsoft.EntityFrameworkCore.Tools
+        public DataBaseContext(DbContextOptions<DataBaseContext> options):base(options)
+        {
+            
+        }
+    }
+}
