350f1b87dcc44e87e8fcc6f3dade9496528c9bdc
Mva <m.vahidassari@gmail.com>
2026-07-17 19:27:10 +0330

    add file for explain

---
+++ Asp.Net-Core/10-Identity/6-LoginUsers/Models/Dto/LoginDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace RunIdentity.Models.Dto
+{
+    public class LoginDto
+    {
+        [Required]
+        [EmailAddress]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name ="Remember Me")]
+        public bool IsPersistent { get; set; } = false;
+        public string ReturnUrl { get; set; }
+    }
+}
