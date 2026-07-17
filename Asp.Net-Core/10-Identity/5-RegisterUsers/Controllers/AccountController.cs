aec8ec3d11ce4bd92c273b38acd24bcede2d2416
Mva <m.vahidassari@gmail.com>
2026-07-17 18:41:24 +0330

    add file for explain

---
+++ Asp.Net-Core/10-Identity/5-RegisterUsers/Controllers/AccountController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RunIdentity.Models.Dto;
+using RunIdentity.Models.Entities;
+using System;
+using System.Linq;
+
+namespace RunIdentity.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+
+        public AccountController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(RegisterDto register)
+        {
+            if (ModelState.IsValid == false)
+            {
+                return View(register);
+            }
+
+            User newUser = new User
+            {
+                FirstName = register.FirstName,
+                LastName = register.LastName,
+                Email = register.Email,
+                UserName=register.Email
+            };
+
+            var result=_userManager.CreateAsync(newUser,register.Password).Result;
+
+            if(result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            string message = "";
+            foreach (var item in result.Errors.ToList())
+            {
+                message += item.Description + Environment.NewLine;
+            }
+
+            TempData["Message"] = message;
+            return View(register);
+        }
+    }
+}
---
+++ Asp.Net-Core/10-Identity/5-RegisterUsers/Models/Dto/RegisterDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RunIdentity.Models.Dto
+{
+    public class RegisterDto
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))] //باعث میشه به صورت خودکار عمل مقایسه رو با پسورد انجام بده
+        public string ConfirmPassword { get; set; }
+    }
+}
