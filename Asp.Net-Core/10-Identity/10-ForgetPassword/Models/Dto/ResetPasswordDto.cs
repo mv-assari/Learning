957721279cc35b82224f3a633661ffb2b1cd551a
Mva <m.vahidassari@gmail.com>
2026-07-25 22:43:58 +0330

    add file for explain

---
+++ Asp.Net-Core/10-Identity/10-ForgetPassword/Models/Dto/ResetPasswordDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RunIdentity.Models.Dto
+{
+    public class ResetPasswordDto
+    {
+        public string UserId { get; set; }
+        public string Token { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+    }
+}
