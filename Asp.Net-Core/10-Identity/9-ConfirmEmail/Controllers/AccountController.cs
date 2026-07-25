39df1f0d1ce40a0cffe139b15d5a71334c660617
Mva <m.vahidassari@gmail.com>
2026-07-25 21:30:04 +0330

    add file for explain

---
+++ Asp.Net-Core/10-Identity/9-ConfirmEmail/Controllers/AccountController.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RunIdentity.Models.Dto;
+using RunIdentity.Models.Entities;
+using RunIdentity.Serivces;
+using System;
+using System.Linq;
+
+namespace RunIdentity.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly EmailService _emailService;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _emailService = new EmailService();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(RegisterDto register)
+        {
+            if (ModelState.IsValid == false)
+            {
+                return View(register);
+            }
+
+            User newUser = new User
+            {
+                FirstName = register.FirstName,
+                LastName = register.LastName,
+                Email = register.Email,
+                UserName = register.Email
+            };
+
+            var result = _userManager.CreateAsync(newUser, register.Password).Result;
+
+            if (result.Succeeded)
+            {
+                var token=_userManager.GenerateEmailConfirmationTokenAsync(newUser).Result;
+                string callBackUrl = Url.Action("ConfirmEmail", "Account", new {UserId=newUser.Id,Token=token},protocol:Request.Scheme);
+
+                string body = $"click link<a href={callBackUrl}>link</a>";
+
+                _emailService.Execute(newUser.Email, body, "active account");
+
+                return RedirectToAction("DisplayEmail");
+            }
+
+            string message = "";
+            foreach (var item in result.Errors.ToList())
+            {
+                message += item.Description + Environment.NewLine;
+            }
+
+            TempData["Message"] = message;
+            return View(register);
+        }
+
+        public IActionResult ConfirmEmail(string userId,string token)
+        {
+            if(userId == null || token == null)
+            {
+                return BadRequest();
+            }
+
+            var user = _userManager.FindByIdAsync(userId).Result;
+
+            var result=_userManager.ConfirmEmailAsync(user, token).Result;
+            return RedirectToAction("Login");
+        }
+
+        public IActionResult DisplayEmail()
+        {
+            return View();
+        }
+
+        public IActionResult Login(string returnUrl = "/")
+        {
+
+            return View(new LoginDto
+            {
+                ReturnUrl = returnUrl
+            });
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginDto login)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(login);
+            }
+
+            var user = _userManager.FindByNameAsync(login.UserName).Result;
+
+            _signInManager.SignOutAsync();
+
+            var result = _signInManager.PasswordSignInAsync(user, login.Password, login.IsPersistent, true).Result;
+
+            if (result.Succeeded == true)
+            {
+                return Redirect(login.ReturnUrl);
+            }
+            if (result.RequiresTwoFactor == true)
+            {
+                //got to twoFactor page
+            }
+            if (result.IsLockedOut == true)
+            {
+                //view error to user and show time to lockedout
+            }
+
+            ModelState.AddModelError(string.Empty, "Login Error");
+
+            return View();
+        }
+
+        public IActionResult LogOut()
+        {
+            _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Asp.Net-Core/10-Identity/9-ConfirmEmail/Serivces/EmailService.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunIdentity.Serivces
+{
+    public class EmailService
+    {
+        public Task Execute(string userEmile,string body,string subject)
+        {
+            SmtpClient client = new SmtpClient();
+            client.Port = 587;
+            client.Host = "smtp.gmail.com";
+            client.EnableSsl = true;
+            client.Timeout = 50000;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            client.Credentials = new NetworkCredential("mv.assari.73@gmail.com", "leds luwu daze jybv"/*در این قسمت به جای رمزعبور ایمیل از بخش https://myaccount.google.com/apppasswords یک رمز عبور میگیریم و دراینجا استفاده میکنیم*/);
+
+            MailMessage message = new MailMessage("mv.assari.73@gmail.com",userEmile,subject,body);
+            message.IsBodyHtml = true;
+            message.BodyEncoding=UTF8Encoding.UTF8;
+            message.DeliveryNotificationOptions=DeliveryNotificationOptions.OnSuccess;
+
+            client.Send(message);
+            return Task.CompletedTask;
+        }
+    }
+}
