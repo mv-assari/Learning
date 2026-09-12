405eff2983b9c3fed94c80d57fd89bc88aab1a24
Mva <m.vahidassari@gmail.com>
2026-09-12 20:00:29 +0330

    add file for explain

---
+++ Asp.Net-Core/10-Identity/12-TwoStepVerification/Models/Dto/MyAccountInfoDto.cs
@@ -0,0 +1,14 @@
+﻿namespace RunIdentity.Models.Dto
+{
+    public class MyAccountInfoDto
+    {
+        public string Id { get; set; }
+        public string FullName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool EmailConfirmed { get; set; }
+        public bool PhoneNumberConfirmed { get; set; }
+        public bool TwoFactorEnabled { get; set; }
+    }
+}
---
+++ Asp.Net-Core/10-Identity/12-TwoStepVerification/Models/Dto/TwoFactroLoginDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RunIdentity.Models.Dto
+{
+    public class TwoFactroLoginDto
+    {
+        [Required]
+        public string Code { get; set; }
+        public bool IsPersistent { get; set; }
+        public string Provider { get; set; }
+    }
+}
