2ae9a5755b904d1df23013467146c64b0d69e65c
Mva <m.vahidassari@gmail.com>
2026-09-12 21:41:30 +0330

    add file for explain

---
+++ Asp.Net-Core/10-Identity/15-PasswordValidator/Helper/MyPasswordValidator.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using RunIdentity.Models.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RunIdentity.Helper
+{
+    public class MyPasswordValidator : IPasswordValidator<User>
+    {
+        List<string> commonPassword = new List<string>()
+        {
+            "123","1234","12345689"
+        };
+        public Task<IdentityResult> ValidateAsync(UserManager<User> manager, User user, string password)
+        {
+            if (commonPassword.Contains(password))
+            {
+                var result = IdentityResult.Failed(new IdentityError
+                {
+                    Code = "CommonPassword",
+                    Description = "این پسورد ضعیف هست کلمه عبور پیچیده تری انتخاب کنید"
+                });
+                return Task.FromResult(result);
+            }
+            return Task.FromResult(IdentityResult.Success);
+        }
+    }
+}
