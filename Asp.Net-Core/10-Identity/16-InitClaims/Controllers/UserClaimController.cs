2cd415429b5292c046a7a25c2182e1a3074def20
Mva <m.vahidassari@gmail.com>
2026-09-13 21:44:17 +0330

    add file for explain

---
+++ Asp.Net-Core/10-Identity/16-InitClaims/Controllers/UserClaimController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RunIdentity.Models.Entities;
+using System.Linq;
+using System.Security.Claims;
+
+namespace RunIdentity.Controllers
+{
+    public class UserClaimController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+
+        public UserClaimController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View(User.Claims);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(string ClaimType,string ClaimValue)
+        {
+            var user = _userManager.GetUserAsync(User).Result;
+
+            Claim newClaim = new Claim(ClaimType, ClaimValue, ClaimValueTypes.String);
+            var result= _userManager.AddClaimAsync(user, newClaim).Result;
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError("", item.Description);
+                }
+            }
+                return View();
+        }
+
+        public IActionResult Delete(string claimType)
+        {
+            var user = _userManager.GetUserAsync(User).Result;
+
+            Claim claim = User.Claims.Where(p => p.Type == claimType).FirstOrDefault();
+            if (claim!=null)
+            {
+                var result = _userManager.RemoveClaimAsync(user, claim).Result;
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Asp.Net-Core/10-Identity/16-InitClaims/Helper/AddMyClaims.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using RunIdentity.Models.Entities;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace RunIdentity.Helper
+{
+    public class AddMyClaims : UserClaimsPrincipalFactory<User>
+    {
+        public AddMyClaims(UserManager<User> userManager,IOptions<IdentityOptions> options):base(userManager, options) { }  
+        
+        protected override async Task<ClaimsIdentity> GenerateClaimsAsync(User user)
+        {
+            var identity=await base.GenerateClaimsAsync(user);
+            identity.AddClaim(new Claim("FullName", $"{user.FirstName} {user.LastName}"));
+
+            return identity;
+        }
+    }
+}
