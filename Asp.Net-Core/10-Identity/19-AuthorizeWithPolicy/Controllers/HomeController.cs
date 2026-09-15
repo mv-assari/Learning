7ee5d23ab303579752289d05f80d4f100fcaf2af
Mva <m.vahidassari@gmail.com>
2026-09-15 21:22:25 +0330

    add file for explain

---
+++ Asp.Net-Core/10-Identity/19-AuthorizeWithPolicy/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RunIdentity.Models;
+
+namespace RunIdentity.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [Authorize(policy:"Buyer")]
+        public string JustBuyer()
+        {
+            return "شما خریدار هستید";
+        }
+
+        [Authorize(policy:"BloodType")]
+        public string Blood()
+        {
+            return "Ap and Op";
+        }
+
+        [Authorize(policy:"Credit")]
+        public string Credit()
+        {
+            return "Ok";
+        }
+    }
+}
---
+++ Asp.Net-Core/10-Identity/19-AuthorizeWithPolicy/Helper/UserCrediteRequerment.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Threading.Tasks;
+
+namespace RunIdentity.Helper
+{
+    public class UserCrediteRequerment:IAuthorizationRequirement
+    {
+        public int Credit {  get; set; }
+
+        public UserCrediteRequerment(int credit)
+        {
+            Credit = credit;
+        }
+    }
+
+    public class UserCreditHandler : AuthorizationHandler<UserCrediteRequerment>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, UserCrediteRequerment requirement)
+        {
+            var claim = context.User.FindFirst("Credit");
+            if (claim != null)
+            {
+                int credit=int.Parse(claim?.Value);
+                if (credit >= requirement.Credit)
+                {
+                    context.Succeed(requirement);
+                }
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
