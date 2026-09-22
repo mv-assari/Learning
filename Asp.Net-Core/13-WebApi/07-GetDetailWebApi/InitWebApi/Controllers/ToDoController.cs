4909952b79c53c37414b9bbd64d391cb3f5e815d
Mva <m.vahidassari@gmail.com>
2026-09-22 06:54:51 +0330

    add file for explain

---
+++ Asp.Net-Core/13-WebApi/07-GetDetailWebApi/InitWebApi/Controllers/ToDoController.cs
@@ -0,0 +1,65 @@
+﻿using InitWebApi.Models.Entities.Dtos;
+using InitWebApi.Models.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace InitWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoController : ControllerBase
+    {
+        private readonly ToDoRepository _toDoRepository;
+
+        public ToDoController(ToDoRepository toDoRepository)
+        {
+            _toDoRepository = toDoRepository;
+        }
+
+        // GET: api/<ToDoController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var todoList= _toDoRepository.GetAll().Select(p=>new ToDoItemDto
+            {
+                Id=p.Id,
+                InsertTime=p.InsertTime,
+                Text=p.Text
+            }).ToList();
+            return Ok(todoList);
+        }
+
+        // GET api/<ToDoController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var todo=_toDoRepository.Get(id);
+            return Ok(new ToDoItemDto
+            {
+                Id = todo.Id,
+                InsertTime = todo.InsertTime,
+                Text = todo.Text
+            });
+        }
+
+        // POST api/<ToDoController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ToDoController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ToDoController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
