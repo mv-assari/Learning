d206e1f2094d78cadf6160e635ba3dcfcc5adc77
Mva <m.vahidassari@gmail.com>
2026-09-22 06:38:17 +0330

    add file for explain

---
+++ Asp.Net-Core/13-WebApi/06-GetListWebApi/InitWebApi/Controllers/ToDoController.cs
@@ -0,0 +1,53 @@
+﻿using InitWebApi.Models.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace InitWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoController : ControllerBase
+    {
+        private readonly ToDoRepository _toDoRepository;
+
+        public ToDoController(ToDoRepository toDoRepository)
+        {
+            _toDoRepository = toDoRepository;
+        }
+
+        // GET: api/<ToDoController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var todoList= _toDoRepository.GetAll();
+            return Ok(todoList);
+        }
+
+        // GET api/<ToDoController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ToDoController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ToDoController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ToDoController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
