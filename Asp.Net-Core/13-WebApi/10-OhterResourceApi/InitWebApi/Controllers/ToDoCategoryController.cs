7b11f3a79c507f71196bad7a5be0bd975a091064
Mva <m.vahidassari@gmail.com>
2026-09-22 21:00:30 +0330

    add file for explain

---
+++ Asp.Net-Core/13-WebApi/10-OhterResourceApi/InitWebApi/Controllers/ToDoCategoryController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InitWebApi.Controllers
+{
+    [Route("api/ToDo/{ToDoId}/Categories/{CategoryId}")]
+    [ApiController]
+    public class ToDoCategoryController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult POST(int ToDoId,int CategoryId)
+        {
+            return Ok();
+        }
+    }
+}
